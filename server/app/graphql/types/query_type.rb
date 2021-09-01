14e8395a6c6d5fca09df4c36327ed946d8d7add8
cjosu <88686663+cjosu@users.noreply.github.com>
2021-09-01 16:11:21 -0400

    Changed GeneType query arguments from ID to name

--- server/app/graphql/types/query_type.rb
+++ server/app/graphql/types/query_type.rb
@@ -8,11 +8,11 @@ module Types
     # They will be entry points for queries on your schema.
     
     field :source, Types::SourceType, null: true do
-      argument :id, String, required: true
+      argument :source_db_name, String, required: true
     end
 
-    def source(id: )
-      Source.find_by(id: id)
+    def source(source_db_name: )
+      Source.find_by(source_db_name: source_db_name)
     end
 
     field :gene_claim, Types::GeneClaimType, null: true do
@@ -24,11 +24,19 @@ module Types
     end
 
     field :gene, Types::GeneType, null: true do
+      argument :name, String, required: true
+    end
+
+    def gene_alias(id: )
+      GeneAlias.find_by(id: id)
+    end
+
+    field :gene_alias, Types::GeneAliasType, null: true do
       argument :id, String, required: true
     end
 
-    def gene(id: )
-      Gene.find_by(id: id)
+    def gene(name: )
+      Gene.find_by(name: name)
     end 
 
   end
