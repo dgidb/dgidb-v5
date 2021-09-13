f0841207a6c2a214df01a5e177edf7c77b5016c3
korikuzma <korikuzma@gmail.com>
2021-09-13 11:50:13 -0400

    has_many relationship should have null: false

--- server/app/graphql/types/gene_claim_type.rb
+++ server/app/graphql/types/gene_claim_type.rb
@@ -8,10 +8,10 @@ module Types
 
     field :gene, Types::GeneType, null: false
     field :gene_claim_categories, [Types::GeneClaimCategoryType], null: false
-    field :gene_claim_aliases, [Types::GeneClaimAliasType], null: true
-    field :gene_claim_attributes, [Types::GeneClaimAttributeType], null: true
+    field :gene_claim_aliases, [Types::GeneClaimAliasType], null: false
+    field :gene_claim_attributes, [Types::GeneClaimAttributeType], null: false
     field :source, Types::SourceType, null: false
-    field :interaction_claims, [Types::InteractionClaimType], null: true
-    field :drug_claims, [Types::DrugClaimType], null: true
+    field :interaction_claims, [Types::InteractionClaimType], null: false
+    field :drug_claims, [Types::DrugClaimType], null: false
   end
 end
--- server/app/graphql/types/gene_type.rb
+++ server/app/graphql/types/gene_type.rb
@@ -5,11 +5,11 @@ module Types
     field :long_name, String, null: true
     field :entrez_id, Int, null: true
 
-    field :gene_claims, [Types::GeneClaimType], null: true
-    field :gene_gene_interaction_claims, [Types::GeneGeneInteractionClaimType], null: true
-    field :interactions, [Types::InteractionType], null: true
-    field :gene_aliases, [Types::GeneAliasType], null: true
-    field :gene_attributes, [Types::GeneAttributeType], null: true
+    field :gene_claims, [Types::GeneClaimType], null: false
+    field :gene_gene_interaction_claims, [Types::GeneGeneInteractionClaimType], null: false
+    field :interactions, [Types::InteractionType], null: false
+    field :gene_aliases, [Types::GeneAliasType], null: false
+    field :gene_attributes, [Types::GeneAttributeType], null: false
     field :gene_categories, [Types::GeneClaimCategoryType], null: false
   end
 end
