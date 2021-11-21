b5a29914945c9bfb7b32765c7630f23bf3fce0f5
korikuzma <korikuzma@gmail.com>
2021-11-21 18:02:35 -0500

    2 space indent, add option for id

--- server/app/graphql/resolvers/genes.rb
+++ server/app/graphql/resolvers/genes.rb
@@ -2,16 +2,15 @@ require 'search_object'
 require 'search_object/plugin/graphql'
 
 class Resolvers::Genes < GraphQL::Schema::Resolver
+  include SearchObject.module(:graphql)
 
-    include SearchObject.module(:graphql)
+  type Types::GeneType.connection_type, null: false
 
-    type Types::GeneType.connection_type, null: false
+  scope { Gene.all }
 
-    scope { Gene.all }
-
-
-    option(:name, type: String) { |scope, value| scope.where("name ILIKE ?", "#{value}%")}
-    option(:long_name, type: String) { |scope, value| scope.where("long_name ILIKE?", "#{value}%")}
-    option(:entrez_id, type: Int) { |scope, value| scope.where(entrez_id: value)}
+  option(:id, type: ID) { |scope, value| scope.where(id: value)}
+  option(:name, type: String) { |scope, value| scope.where("name ILIKE ?", "#{value}%")}
+  option(:long_name, type: String) { |scope, value| scope.where("long_name ILIKE?", "#{value}%")}
+  option(:entrez_id, type: Int) { |scope, value| scope.where(entrez_id: value)}
 
 end
