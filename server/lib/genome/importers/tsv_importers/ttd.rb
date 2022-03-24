6110cf1d952e429dcbdf18bfac336123eb49f148
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-03-24 13:04:03 -0400

    Style fixes

--- server/lib/genome/importers/tsv_importers/ttd.rb
+++ server/lib/genome/importers/tsv_importers/ttd.rb
@@ -1,6 +1,7 @@
-module Genome; module Importers; module TsvImporters; module TTD;
+module Genome; module Importers; module TsvImporters; module Ttd;
   class Importer < Genome::Importers::Base
     attr_reader :file_path
+
     def initialize(file_path)
       @file_path = file_path
       @source_db_name = 'TTD'
@@ -14,14 +15,14 @@ module Genome; module Importers; module TsvImporters; module TTD;
     def create_new_source
       @source ||= Source.create(
         {
-          base_url:          'http://db.idrblab.net/ttd/',
-          site_url:          'http://bidd.nus.edu.sg/group/cjttd/',
-          citation:          "Wang,Y., Zhang,S., Li,F., Zhou,Y., Zhang,Y., Wang,Z., Zhang,R., Zhu,J., Ren,Y., Tan,Y., et al. (2020) Therapeutic target database 2020: enriched resource for facilitating research and early development of targeted therapeutics. Nucleic Acids Res., 48, D1031–D1041. PMID: 31691823",
-          source_db_version:  '2020.06.01',
-          source_db_name:    source_db_name,
-          full_name:         'Therapeutic Target Database',
+          base_url: 'http://db.idrblab.net/ttd/',
+          site_url: 'http://bidd.nus.edu.sg/group/cjttd/',
+          citation: 'Wang,Y., Zhang,S., Li,F., Zhou,Y., Zhang,Y., Wang,Z., Zhang,R., Zhu,J., Ren,Y., Tan,Y., et al. (2020) Therapeutic target database 2020: enriched resource for facilitating research and early development of targeted therapeutics. Nucleic Acids Res., 48, D1031–D1041. PMID: 31691823',
+          source_db_version: '2020.06.01',
+          source_db_name: source_db_name,
+          full_name: 'Therapeutic Target Database',
           license: 'Unclear. Website states "All Rights Reserved" but resource structure and description in 2002 publication indicate "open-access".',
-          license_link: 'https://academic.oup.com/nar/article/30/1/412/1331814',
+          license_link: 'https://academic.oup.com/nar/article/30/1/412/1331814'
         }
       )
       @source.source_types << SourceType.find_by(type: 'interaction')
@@ -30,7 +31,9 @@ module Genome; module Importers; module TsvImporters; module TTD;
 
     def import_claims
       CSV.foreach(file_path, :headers => true, :col_sep => ",") do |row|
-        next if row['Highest_status'] == 'Terminated' || row['Highest_status'] == 'Withdrawn from market' || row['Highest_status'].match(/Discontinued/) || row['Highest_status'] == 'Investigative'
+        if row['Highest_status'] == 'Terminated' || row['Highest_status'] == 'Withdrawn from market' || row['Highest_status'].match(/Discontinued/) || row['Highest_status'] == 'Investigative'
+          next
+        end
 
         gene_name, gene_abbreviation = row['Target_Name'].split(' (', 2)
         gene_abbreviation.sub!(')', '')
@@ -46,23 +49,17 @@ module Genome; module Importers; module TsvImporters; module TTD;
           create_interaction_claim_type(interaction_claim, Genome::Normalizers::InteractionClaimType.name_normalzier(row['MOA']))
         end
 
-        #this is not a typo but the actual column header from the source TSV
-        if !row['Referecnce'].nil?
+        # this is not a typo but the actual column header from the source TSV
+        unless row['Referecnce'].nil?
           row['Referecnce'].split('; ').each do |ref|
             if ref.include? 'pubmed'
-              url, sep, pmid = ref.rpartition('/')
-              if pmid == ''
-                pmid = url.rpartition('/').last
-              end
-              if !pmid == '217546303'
-                create_interaction_claim_publication(interaction_claim, pmid)
-              end
+              url, _sep, pmid = ref.rpartition('/')
+              pmid = url.rpartition('/').last if pmid == ''
+              create_interaction_claim_publication(interaction_claim, pmid) if !pmid == '217546303'
             end
             if ref.include? 'pmc'
-              url, sep, pmcid = ref.rpartition('/')
-              if pmcid == ''
-                pmcid = url.rpartition('/').last
-              end
+              url, _sep, pmcid = ref.rpartition('/')
+              pmcid = url.rpartition('/').last if pmcid == ''
               create_interaction_claim_publication_by_pmcid(interaction_claim, pmcid)
             end
           end
@@ -70,7 +67,7 @@ module Genome; module Importers; module TsvImporters; module TTD;
 
         create_interaction_claim_link(interaction_claim, 'TTD Target Information', "http://idrblab.net/ttd/data/target/details/#{row['TargetID']}")
       end
-      backfill_publication_information()
+      backfill_publication_information
     end
   end
 end; end; end; end;
