5630a1c4944112c51b9c4f141c6df12d78011e4b
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-03-28 11:41:58 -0400

    Handle weird output encoding

--- server/lib/genome/importers/tsv_importers/tdg_clinical_trial.rb
+++ server/lib/genome/importers/tsv_importers/tdg_clinical_trial.rb
@@ -13,17 +13,18 @@ module Genome; module Importers; module TsvImporters; module TdgClinicalTrial;
     end
 
     private
+
     def create_new_source
       @source ||= Source.create(
         {
-          base_url: 'https://clinicaltrials.gov/ct2/results?Search=Search&term=',  # TODO: is this correct?
+          base_url: 'https://clinicaltrials.gov/ct2/results?Search=Search&term=', # TODO: is this correct?
           site_url: 'http://www.ncbi.nlm.nih.gov/pubmed/24016212',
           citation: 'The druggable genome: Evaluation of drug targets in clinical trials suggests major shifts in molecular class and indication. Rask-Andersen M, Masuram S, Schioth HB. Annu Rev Pharmacol Toxicol. 2014;54:9-26. doi: 10.1146/annurev-pharmtox-011613-135943. PMID: 24016212',
           source_db_version: 'Jan-2014',
           source_db_name: source_db_name,
           full_name: 'The Druggable Genome: Evaluation of Drug Targets in Clinical Trials Suggests Major Shifts in Molecular Class and Indication (Rask-Andersen, Masuram, Schioth 2014)',
           license: 'Supplementary table from Annual Reviews copyright publication',
-          license_link: 'https://www.annualreviews.org/doi/10.1146/annurev-pharmtox-011613-135943?url_ver=Z39.88-2003&rfr_id=ori%3Arid%3Acrossref.org&rfr_dat=cr_pub++0pubmed'  # TODO: not sure what this is supposed to link to
+          license_link: 'https://www.annualreviews.org/doi/10.1146/annurev-pharmtox-011613-135943?url_ver=Z39.88-2003&rfr_id=ori%3Arid%3Acrossref.org&rfr_dat=cr_pub++0pubmed' # TODO: not sure what this is supposed to link to
         }
       )
       @source.source_types << SourceType.find_by(type: 'interaction')
@@ -31,14 +32,16 @@ module Genome; module Importers; module TsvImporters; module TdgClinicalTrial;
     end
 
     def create_interaction_claims
-      CSV.foreach(file_path, headers: true, col_sep: "\t") do |row|
+      CSV.foreach(file_path, headers: true, col_sep: "\t", encoding: 'iso-8859-1:utf-8') do |row|
         gene_claim = create_gene_claim(row['Uniprot Accession number'], 'Uniprot Accession')
         create_gene_claim_alias(gene_claim, row['Gene'], 'Gene Symbol')
         unless row['Target main class'].blank?
           create_gene_claim_attribute(gene_claim, 'Target Class', row['Target main class'])
         end
-        row['Target class'].split(';').each do |subclass|
-          create_gene_claim_attribute(gene_claim, 'Target Subclass', subclass)
+        unless row['Target class'].nil?
+          row['Target class'].split(';').each do |subclass|
+            create_gene_claim_attribute(gene_claim, 'Target Subclass', subclass)
+          end
         end
 
         drug_claim = create_drug_claim(row['Drug Name'].upcase, row['Drug Name'].upcase, 'Drug Name')
