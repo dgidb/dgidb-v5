c02e45343813dd2a03ada595ee02fcbeaea51da9
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-03-28 10:13:41 -0400

    Style fixes

--- server/lib/genome/importers/tsv_importers/nci.rb
+++ server/lib/genome/importers/tsv_importers/nci.rb
@@ -12,6 +12,7 @@ module Genome; module Importers; module TsvImporters; module Nci;
     end
 
     private
+
     def create_new_source
       @source ||= Source.create(
         {
@@ -30,7 +31,7 @@ module Genome; module Importers; module TsvImporters; module Nci;
     end
 
     def create_interaction_claims
-      CSV.foreach(file_path, encoding:'iso-8859-1:utf-8', :headers => true, :col_sep => "\t") do |row|
+      CSV.foreach(file_path, encoding:'iso-8859-1:utf-8', headers: true, col_sep: "\t") do |row|
         gene_claim = create_gene_claim(row['Gene'], 'CGI Gene Name')
         drug = row['Drug'].upcase
         drug_claim = create_drug_claim(drug, drug, 'NCI Drug Name')
