bc7b9b3f0a94701a555fa049a6f8e90881bcf895
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-03-28 13:29:51 -0400

    Add file input notes

--- server/lib/genome/importers/tsv_importers/cgi.rb
+++ server/lib/genome/importers/tsv_importers/cgi.rb
@@ -1,6 +1,8 @@
 module Genome; module Importers; module TsvImporters; module Cgi
+  # 90% certain that we want `cgi_biomarkers.tsv`
   class Importer < Genome::Importers::Base
     attr_reader :file_path
+
     def initialize(file_path)
       @file_path = file_path
       @source_db_name = 'CGI'
@@ -30,8 +32,9 @@ module Genome; module Importers; module TsvImporters; module Cgi
     end
 
     def create_interaction_claims
-      CSV.foreach(file_path, :headers => true, :col_sep => "\t") do |row|
+      CSV.foreach(file_path, headers: true, col_sep: "\t") do |row|
         next if row['Drug'].nil? || row['Drug'] == '[]'
+
         if row['Drug'].include?(',') || row['Drug'].include?(';')
           combination_drug_name = row['Drug']
           combination_drug_name.scan(/[a-zA-Z0-9]+/).each do |individual_drug_name|
@@ -43,9 +46,7 @@ module Genome; module Importers; module TsvImporters; module Cgi
                 create_interaction_claim_attribute(interaction_claim, 'combination therapy', combination_drug_name)
                 create_interaction_claim_attribute(interaction_claim, 'Drug family', row['Drug family'])
                 create_interaction_claim_attribute(interaction_claim, 'Alteration', row['Alteration'])
-                if row['Source'].include?('PMID')
-                  add_interaction_claim_publications(interaction_claim, row['Source'])
-                end
+                add_interaction_claim_publications(interaction_claim, row['Source']) if row['Source'].include?('PMID')
                 create_interaction_claim_link(interaction_claim, 'Cancer Biomarkers database', 'https://www.cancergenomeinterpreter.org/biomarkers')
               end
             else
@@ -60,35 +61,31 @@ module Genome; module Importers; module TsvImporters; module Cgi
               create_interaction_claim_link(interaction_claim, 'Cancer Biomarkers database', 'https://www.cancergenomeinterpreter.org/biomarkers')
             end
           end
-        if row['Drug'].include?(';')
-          combination_drug_name = row['Drug']
-          combination_drug_name.split(';').each do |individual_drug_name|
-            drug_claim = create_drug_claim(individual_drug_name, individual_drug_name, 'CGI Drug Name')
-            if row['Gene'].include?(';')
-              row['Gene'].split(';').each do |indv_gene|
-                gene_claim = create_gene_claim(indv_gene, 'CGI Gene Name')
+          if row['Drug'].include?(';')
+            combination_drug_name = row['Drug']
+            combination_drug_name.split(';').each do |individual_drug_name|
+              drug_claim = create_drug_claim(individual_drug_name, individual_drug_name, 'CGI Drug Name')
+              if row['Gene'].include?(';')
+                row['Gene'].split(';').each do |indv_gene|
+                  gene_claim = create_gene_claim(indv_gene, 'CGI Gene Name')
+                  interaction_claim = create_interaction_claim(gene_claim, drug_claim)
+                  create_interaction_claim_attribute(interaction_claim, 'combination therapy', combination_drug_name)
+                  create_interaction_claim_attribute(interaction_claim, 'Drug family', row['Drug family'])
+                  create_interaction_claim_attribute(interaction_claim, 'Alteration', row['Alteration'])
+                  add_interaction_claim_publications(interaction_claim, row['Source']) if row['Source'].include?('PMID')
+                  create_interaction_claim_link(interaction_claim, 'Cancer Biomarkers database', 'https://www.cancergenomeinterpreter.org/biomarkers')
+                end
+              else
+                gene_claim = create_gene_claim(row['Gene'], 'CGI Gene Name')
                 interaction_claim = create_interaction_claim(gene_claim, drug_claim)
                 create_interaction_claim_attribute(interaction_claim, 'combination therapy', combination_drug_name)
                 create_interaction_claim_attribute(interaction_claim, 'Drug family', row['Drug family'])
                 create_interaction_claim_attribute(interaction_claim, 'Alteration', row['Alteration'])
-                if row['Source'].include?('PMID')
-                  add_interaction_claim_publications(interaction_claim, row['Source'])
-                end
+                add_interaction_claim_publications(interaction_claim, row['Source']) if row['Source'].include?('PMID')
                 create_interaction_claim_link(interaction_claim, 'Cancer Biomarkers database', 'https://www.cancergenomeinterpreter.org/biomarkers')
               end
-            else
-              gene_claim = create_gene_claim(row['Gene'], 'CGI Gene Name')
-              interaction_claim = create_interaction_claim(gene_claim, drug_claim)
-              create_interaction_claim_attribute(interaction_claim, 'combination therapy', combination_drug_name)
-              create_interaction_claim_attribute(interaction_claim, 'Drug family', row['Drug family'])
-              create_interaction_claim_attribute(interaction_claim, 'Alteration', row['Alteration'])
-              if row['Source'].include?('PMID')
-                add_interaction_claim_publications(interaction_claim, row['Source'])
-              end
-              create_interaction_claim_link(interaction_claim, 'Cancer Biomarkers database', 'https://www.cancergenomeinterpreter.org/biomarkers')
             end
           end
-        end
         else
           drug_claim = create_drug_claim(row['Drug'], row['Drug'], 'CGI Drug Name')
           if row['Gene'].include?(';')
@@ -97,9 +94,7 @@ module Genome; module Importers; module TsvImporters; module Cgi
               interaction_claim = create_interaction_claim(gene_claim, drug_claim)
               create_interaction_claim_attribute(interaction_claim, 'Drug family', row['Drug family'])
               create_interaction_claim_attribute(interaction_claim, 'Alteration', row['Alteration'])
-              if row['Source'].include?('PMID')
-                add_interaction_claim_publications(interaction_claim, row['Source'])
-              end
+              add_interaction_claim_publications(interaction_claim, row['Source']) if row['Source'].include?('PMID')
               create_interaction_claim_link(interaction_claim, 'Cancer Biomarkers database', 'https://www.cancergenomeinterpreter.org/biomarkers')
             end
           else
@@ -107,23 +102,19 @@ module Genome; module Importers; module TsvImporters; module Cgi
             interaction_claim = create_interaction_claim(gene_claim, drug_claim)
             create_interaction_claim_attribute(interaction_claim, 'Drug family', row['Drug family'])
             create_interaction_claim_attribute(interaction_claim, 'Alteration', row['Alteration'])
-            if row['Source'].include?('PMID')
-              add_interaction_claim_publications(interaction_claim, row['Source'])
-            end
+            add_interaction_claim_publications(interaction_claim, row['Source']) if row['Source'].include?('PMID')
             create_interaction_claim_link(interaction_claim, 'Cancer Biomarkers database', 'https://www.cancergenomeinterpreter.org/biomarkers')
           end
         end
       end
-      backfill_publication_information()
+      backfill_publication_information
     end
 
     def add_interaction_claim_publications(interaction_claim, source_string)
       if source_string.include?(';')
         source_string.split(';').each do |value|
           value.split(/[^\d]/).each do |pmid|
-            unless pmid.nil? || pmid == ''
-              create_interaction_claim_publication(interaction_claim, pmid)
-            end
+            create_interaction_claim_publication(interaction_claim, pmid) unless pmid.nil? || pmid == ''
           end
         end
       else
