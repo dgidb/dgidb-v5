dac0bbcfda5efdaf3b8317d41d6df212515d4fe8
cjosu <88686663+cjosu@users.noreply.github.com>
2022-04-15 13:18:33 -0400

    Styling results page

--- client/src/components/Gene Summary/GeneSummary.tsx
+++ client/src/components/Gene Summary/GeneSummary.tsx
@@ -6,6 +6,21 @@ import { GlobalClientContext } from 'stores/Global/GlobalClient';
 // styles
 import './GeneSummary.scss';
 
+const InteractionCount: React.FC = () => {
+  return (
+    <div className="interaction-count-container">
+      <h4>Gene Interactions</h4>
+    </div>
+  )
+}
+const SummaryInfo: React.FC = () => {
+  return (
+    <div className="summary-infographic-container">
+      <h4>Summary Infographics</h4>
+    </div>
+  )
+}
+
 export const GeneSummary: React.FC = () => {
 
   const {state} = useContext(GlobalClientContext);
@@ -24,7 +39,10 @@ export const GeneSummary: React.FC = () => {
   return (
     <div className="gene-summary-container">
       <h3>Gene Summary</h3>
-
+      <div className="gene-summary-content">
+        <InteractionCount />
+        <SummaryInfo />
+      </div>
     </div>
   )
 };
