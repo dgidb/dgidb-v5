90b46edf409fbca036866922a300ce997bc141b2
cjosu <88686663+cjosu@users.noreply.github.com>
2022-05-02 15:31:44 -0400

    Adding gene interaction chart columns

--- client/src/components/Interaction Table/Gene/InteractionTable.tsx
+++ client/src/components/Interaction Table/Gene/InteractionTable.tsx
@@ -46,17 +46,40 @@ export const InteractionTable: React.FC = () => {
       )
     },
     {
-      title: 'Approved',
+      title: 'Approval Status',
       dataIndex: ['drug', 'approved'],
       render: (text: any, record: any) => (
         <span>{record?.drug?.approved ? 'Approved' : 'Not Approved'}</span>
       )
     },
     {
+      title: 'Indication',
+      dataIndex: ['drug', 'drugAttributes'],
+      render: (text: any, record: any) => {
+        let drugAttributes = record?.drug?.drugAttributes;
+
+        let indicationAttributes = drugAttributes.filter((attribute: any) => {
+          return attribute.name === 'Drug Indications'
+        })
+
+        return indicationAttributes.map((attribute: any) => {
+          return <span>{attribute.value}</span>
+        })
+
+      }
+    },
+    {
       title: 'Interaction Score',
       dataIndex: ['interactionScore'],
       render: (text: any, record: any) => (
         <span>{truncateDecimals(record?.interactionScore, 2)}</span>
+      )
+    },
+    {
+      title: 'Query Score',
+      dataIndex: ['queryScore'],
+      render: (text: any, record: any) => (
+        <span></span>
       )
     },
   ]
