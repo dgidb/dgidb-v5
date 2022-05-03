a8de9ec7d13d282cc818da9649f346133156348c
mcannon068nw <Matthew.Cannon2@nationwidechildrens.org>
2022-05-03 12:42:09 -0400

    PR requested changes

--- server/lib/genome/importers/file_importers/drugbank.rb
+++ server/lib/genome/importers/file_importers/drugbank.rb
@@ -19,13 +19,13 @@ module Genome; module Importers; module FileImporters; module Drugbank;
     def create_new_source
       @source ||= Source.create(
         {
-            base_url: 'https://go.drugbank.com/',
+            base_url: 'https://go.drugbank.com/drugs',
             site_url: 'https://go.drugbank.com/',
             citation: "DrugBank 5.0: a major update to the DrugBank database for 2018. Wishart DS, Feunang YD, Guo AC, Lo EJ, Marcu A, Grant JR, Sajed T, Johnson D, Li C, Sayeeda Z, Assempour N, Iynkkaran I, Liu Y, Maciejewski A, Gale N, Wilson A, Chin L, Cummings R, Le D, Pon A, Knox C, Wilson M. Nucleic Acids Res. 2017 Nov 8. doi 10.1093/nar/gkx1037. PubMed ID: 29126136",
             source_db_version: '5.1.9',
             source_db_name: 'DrugBank',
             full_name: 'DrugBank - Open Data Drug & Drug Target Database',
-            license: '',
+            license: 'Custom non-commercial',
             license_link: 'https://dev.drugbankplus.com/guides/drugbank/citing?_ga=2.29505343.1251048939.1591976592-781844916.1591645816'
         }
       )
@@ -34,7 +34,6 @@ module Genome; module Importers; module FileImporters; module Drugbank;
     end
 
     def create_claims
-        @i_failure = 0
         @drug_filter.all_records.each do |record|
 
             if record[2].exclude?"n/a"
@@ -55,14 +54,10 @@ module Genome; module Importers; module FileImporters; module Drugbank;
                 record[4].each do |pmid|
                     create_interaction_claim_publication(interaction_claim, pmid)
                 end
-
             end
-
         end
         backfill_publication_information
     end
-
-
   end
 
   class DrugFilter < Nokogiri::XML::SAX::Document
@@ -129,7 +124,6 @@ module Genome; module Importers; module FileImporters; module Drugbank;
             @current_target_name = 'n/a'
             @current_target_action = 'n/a'
 
-
         when 'action'
             @target_action_flag = true
 
@@ -141,7 +135,6 @@ module Genome; module Importers; module FileImporters; module Drugbank;
             @current_target_gene_identifiers = []
             @current_target_gene_id_resources = []
 
-
         when 'identifier'
             if @is_gene_identifier
                 @target_gene_identifier_flag = true
@@ -152,9 +145,6 @@ module Genome; module Importers; module FileImporters; module Drugbank;
                 @target_gene_id_resource_flag = true
             end
         end
-
-
-
     end
 
     def characters(string)
@@ -162,7 +152,6 @@ module Genome; module Importers; module FileImporters; module Drugbank;
         if @drugbank_id_flag
             @drugbank_id_flag = false
             if is_not_salts
-                # p 'DRUGBANK ID: ' + string
                 @current_drug_id = string
             end
         end
@@ -171,7 +160,6 @@ module Genome; module Importers; module FileImporters; module Drugbank;
             if is_not_salts
                 @name_flag = false
                 @is_primary_drug = false
-                # p 'DRUG NAME: ' + string unless string.include?"\n"
                 @current_drug_name = string
             end
         end
@@ -179,7 +167,6 @@ module Genome; module Importers; module FileImporters; module Drugbank;
         if @target_name_flag
             if @is_target
                 @target_name_flag = false
-                # p 'TARGET NAME: ' + string unless string.include?"\n"
                 @current_target_name = string
             end
         end
@@ -187,7 +174,6 @@ module Genome; module Importers; module FileImporters; module Drugbank;
         if target_action_flag
             if is_target
                 @target_action_flag = false
-                # p 'ACTION TYPE: ' + string unless string.include?"\n"
                 @current_target_action = string
             end
         end
@@ -195,7 +181,6 @@ module Genome; module Importers; module FileImporters; module Drugbank;
         if target_pubmed_flag
             if is_target
                 @target_pubmed_flag = false
-                # p 'PUBMED ID: ' + string unless string.include?"\n"
                 @current_target_pmid.append(string) unless string.include?"\n"
             end
         end
@@ -213,7 +198,6 @@ module Genome; module Importers; module FileImporters; module Drugbank;
                 @current_target_gene_id_resources.append(string) unless string.include?"\n"
             end
         end
-
     end
 
     def end_element(name)
@@ -230,17 +214,13 @@ module Genome; module Importers; module FileImporters; module Drugbank;
             @record.append(current_drug_id,@current_drug_name,@current_target_name,@current_target_action,@current_target_pmid,@current_target_gene_identifiers,@current_target_gene_id_resources)
             @all_records.append(@record)
             @record = []
-            #append all sections to a record
 
         when 'targets'
         end
-
     end
 
     def end_document
-        p 'Finished parsing'
     end
 
   end
-
 end;end; end; end
