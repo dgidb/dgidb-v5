1e7c41d28c2aea19329521f305a467e87e80f3e3
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-05-12 12:24:44 -0400

    Re-add fresh results and home pages

---
+++ client/src/pages/Results/results.page.tsx
@@ -0,0 +1,48 @@
+// hooks/dependencies
+import React, {useContext, useEffect} from 'react';
+import { GlobalClientContext } from 'stores/Global/GlobalClient';
+import { useNavigate } from 'react-router-dom';
+
+// components
+import { InteractionTable } from 'components/Interaction Table/Gene';
+import { GeneSummary } from 'components/Interaction Summary/Gene';
+
+// styles
+import './results.page.scss';
+import { DrugSummary } from '../../components/Interaction Summary/Drug/DrugSummary';
+
+const GeneResults: React.FC = () => {
+  return (
+    <>
+      <GeneSummary />
+      <InteractionTable />
+    </>
+  )
+}
+
+const DrugResults: React.FC = () => {
+  return (
+    <>
+      <DrugSummary />
+    </>
+  )
+}
+
+export const Results: React.FC = () => {
+  const {state} = useContext(GlobalClientContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(!state.searchTerms.length) {
+      navigate('/home');
+    }
+  }, [])
+
+  return (
+    <div className="results-page-container">
+      {state.interactionMode === 'gene' && <GeneResults />}
+      {state.interactionMode === 'drug' && <DrugResults />}
+    </div>
+  )
+};
+
