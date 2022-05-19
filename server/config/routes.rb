87b29c8728821a97dc01327673aac1733214e186
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-05-19 17:07:11 -0400

    Move endpoints behind /api/ path

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post "/api/graphql", to: "graphql#execute"
 
-  get '/health_check', to: proc { [200, {}, ['success']] } unless Gene.first.nil?
+  get '/api/health_check', to: proc { [200, {}, ['success']] } unless Gene.first.nil?
 
-  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/api/graphql"
+  mount GraphiQL::Rails::Engine, at: "/api/graphiql", graphql_path: "/api/graphql"
 end
