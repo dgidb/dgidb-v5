65ca8f5a3cabbe40d2cd8c5a553238212677241b
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-05-19 12:55:55 -0400

    Call DB in health check

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post "/api/graphql", to: "graphql#execute"
 
-  get '/health_check', to: proc { [200, {}, ['success']] }
+  get '/health_check', to: proc { [200, {}, ['success']] } unless Gene.first.nil?
 
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/api/graphql"
 end
