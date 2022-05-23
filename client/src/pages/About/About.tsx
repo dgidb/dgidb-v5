294cde6d75dcecbe9b56a7c3617308e9643ee9c3
cjosu <88686663+cjosu@users.noreply.github.com>
2022-05-23 15:13:07 -0400

    Adding anchor component to About page

--- client/src/pages/About/About.tsx
+++ client/src/pages/About/About.tsx
@@ -3,12 +3,22 @@ import React, { useState, useContext, useEffect} from 'react';
 
 // styles
 import './About.scss';
+import { Anchor } from 'antd';
+
+const { Link } = Anchor;
 
 export const About = () => {
 
   return(
     <div className="about-page-container">
-      About!
+      <Anchor affix={false}>
+        <Link href="#components-anchor-demo-basic" title="Basic demo" />
+        <Link href="#components-anchor-demo-static" title="Static demo" />
+        <Link href="#API" title="API">
+          <Link href="#Anchor-Props" title="Anchor Props" />
+          <Link href="#Link-Props" title="Link Props" />
+        </Link>
+      </Anchor>
     </div>
   )
 }
