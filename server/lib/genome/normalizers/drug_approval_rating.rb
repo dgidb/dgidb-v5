3046ecd9a0e7b6a611b9d79b32132a46557fafee
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-05-22 20:41:06 -0400

    Add normalizer method

---
+++ server/lib/genome/normalizers/drug_approval_rating.rb
@@ -0,0 +1,27 @@
+module Genome
+  module Normalizers
+    class DrugApprovalRatingNormalizer
+      def self.normalize_approval_ratings
+        approved_drugs = retrieve_approved_drugs
+        puts "Found #{approved_drugs.size} total approved drugs"
+        approved_drugs.each do |drug|
+          unless drug.approved
+            drug.approved = true
+            drug.save
+          end
+        end
+      end
+
+      private_class_method def self.retrieve_approved_drugs
+        Drug.includes(:drug_approval_ratings).where('drug_approval_ratings.rating' => approved_values)
+      end
+
+      private_class_method def self.approved_values
+        [
+          'Withdrawn', 'Approved, Withdrawn', 'Prescription', 'Approved', 'Max Phase 4',
+          'Over-the-Counter', 'Withdrawn', 'Approved', 'Approved before 1982', 'Prescribable'
+        ]
+      end
+    end
+  end
+end
