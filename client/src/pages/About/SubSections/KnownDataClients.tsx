11d7026d1f8bdab40a5375a465df23e83ed39646
cjosu <88686663+cjosu@users.noreply.github.com>
2022-06-02 18:08:10 -0400

    Known Data Clients

---
+++ client/src/pages/About/SubSections/KnownDataClients.tsx
@@ -0,0 +1,36 @@
+export const Overview = () => {
+
+  return(
+    <div className="about-section-container doc-section">
+
+      Data clients are academic and commercial resources that use DGIdb data (for example by integrating DGIdb interactions into their own tool). The current list of DGIdb data clients includes:
+
+      BioGPS
+      CancerGD
+      CancerTracer
+      CANCROX
+      CoDReS
+      CSgator
+      GEMINI - GEnome MINIng
+      Gene4Denovo
+      GeneCards
+      GMIEC - a shiny app
+      Hemap
+      mTCTScan
+      NCG - Network of Cancer Genes
+      NDEx
+      OncoGemini
+      PANDA (Pathway AND Annotation) Explorer
+      Personalized Cancer Therapy
+      TargetDB
+      SL-BioDP
+      SwissMTB
+      theRING
+      TCLP - TRON Cell Line Portal
+      VarCards
+      VAReporter
+
+      If your resource is using DGIdb but is missing from this list and you would like us to add it, please contact us at help@dgidb.org.
+    </div>
+  )
+}
