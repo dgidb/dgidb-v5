0aac55afe418b05b6dcc6274d95cd13c1ff920a3
cjosu <88686663+cjosu@users.noreply.github.com>
2022-06-13 00:32:07 -0400

    Adding tabs to Category Results

--- client/src/components/Gene/Categories/CategoryResults.tsx
+++ client/src/components/Gene/Categories/CategoryResults.tsx
@@ -7,45 +7,60 @@ import { useGetCategories} from 'hooks/queries/useGetCategories';
 import { GlobalClientContext } from 'stores/Global/GlobalClient';
 
 // styles
+import { Tabs } from 'antd';
 import './CategoryResults.scss';
 
 export const CategoryResults: React.FC = () => {
+
+  const { TabPane } = Tabs;
 
   const { state } = useContext(GlobalClientContext);
   const { data } = useGetCategories(state.searchTerms);
 
   const genes = data?.genes
 
+  const onChange = () => {
+
+  }
+
   return (
     <div className="category-results-container">
-      <div className="gene-categories">
-        {genes?.map((gene: any) => {
-          return (
-            <div className="gene-category-result">
-            <div className="gene-header">Search Term(s): "{gene.name}"</div>
-            <div className="category-info">
-            <div className="category-item">
-              <div className="category-name table-header">Category</div>
-              <div className="sources-by-category table-header">Sources</div>
-            </div>
-            {gene.geneCategoriesWithSources.map((cat: any) => {
+      <Tabs defaultActiveKey="1" onChange={onChange}>
+        <TabPane tab="Unique Matches" key="1">
+        <div className="gene-categories">
+            {genes?.map((gene: any) => {
               return (
+                <div className="gene-category-result">
+                <div className="gene-header">Search Term(s): "{gene.name}"</div>
+                <div className="category-info">
                 <div className="category-item">
-                  <div className="category-name">{cat.name}</div>
-                  <div className="sources-by-category">{cat.sourceNames.map((srcname: any, index: number) => {
-                    if(index === cat.sourceNames.length - 1) {
-                      return <span>{srcname}</span>
-                    }
-                    return <span>{srcname}, </span>
-                  })}</div>
+                  <div className="category-name table-header">Category</div>
+                  <div className="sources-by-category table-header">Sources</div>
                 </div>
-              )
+                {gene.geneCategoriesWithSources.map((cat: any) => {
+                  return (
+                    <div className="category-item">
+                      <div className="category-name">{cat.name}</div>
+                      <div className="sources-by-category">{cat.sourceNames.map((srcname: any, index: number) => {
+                        if(index === cat.sourceNames.length - 1) {
+                          return <span>{srcname}</span>
+                        }
+                        return <span>{srcname}, </span>
+                      })}</div>
+                    </div>
+                  )
+                })}
+                </div>
+              </div>
+                )
             })}
-            </div>
           </div>
-            )
-        })}
-      </div>
+        </TabPane>
+        <TabPane tab="Ambiguous or Unmatched" key="2">
+          Content of Tab Pane 2
+        </TabPane>
+      </Tabs>
+
     </div>
   )
 };
