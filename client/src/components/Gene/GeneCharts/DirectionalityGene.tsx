2601b9937f42b7ed7dfdd8a10af0b96faec87328
cjosu <88686663+cjosu@users.noreply.github.com>
2022-06-15 14:57:22 -0400

    Directionality Chart now loads data + filters by gene

--- client/src/components/Gene/GeneCharts/DirectionalityGene.tsx
+++ client/src/components/Gene/GeneCharts/DirectionalityGene.tsx
@@ -8,16 +8,18 @@ import { Pie } from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const DirectionalityGene: React.FC = () => {
-  const {state} = useContext(GlobalClientContext);
-  const { data } = useGetInteractionsByGenes(state.searchTerms);
+interface Props {
+  data: any;
+}
+
+export const DirectionalityGene: React.FC<Props> = ({data}) => {
+  const [count, setCount] = useState<number[]>([0, 0, 0]);
 
   const [chartData, setChartData] = useState<any>({
     labels: ['Activating', 'Inhibiting', 'N/A'],
     datasets: [
       {
-        label: 'Dataset 1',
-        data: [67, 32, 21],
+        data: [0, 0, 0],
         backgroundColor: ['#480A77', '#8075FF', '#89E8F1', '#FA198B', '#4BC6B9', '#F0EFF4', '#D1CFE2', '#BAA898'],
       }
     ]
@@ -29,67 +31,34 @@ export const DirectionalityGene: React.FC = () => {
 
   const labels = ['Activating', 'Inhibiting', 'N/A'];
 
-  // useEffect(() => {
-  //   if (data?.genes?.length) {
-  //     data.genes.forEach((gene: any) => {
-  //       let dataArray = [0, 0, 0]
-  //       gene.interactions.forEach((int: any) => {
-  //           switch(int.interactionTypes[0].directionality){
-  //             case 0:
-  //               dataArray[0]++;
-  //               break;
-  //             case 1:
-  //               dataArray[1]++;
-  //               break;
-  //             case null:
-  //               dataArray[2]++;
-  //               break;
-  //             default:
-  //               return;
-  //           }
-  //       })
-
-  //       setChartData({
-  //         labels,
-  //         datasets: [
-  //           {
-  //             label: 'Dataset 1',
-  //             data: dataArray,
-  //             backgroundColor: 'rgba(255, 99, 132, 0.5)',
-  //           }
-  //         ]
-  //       });
-  //     })
-  //   }
-  // }, [data])
-
-  const data2 = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
-
+  useEffect(() => {
+    let countCopy = [0, 0, 0];
+    data?.forEach((gene: any) => {
+      gene?.interactions?.forEach((int: any) => {
+        if (!int.interactionTypes.length) {
+          countCopy[2] = count[2]++
+        } else {
+          int.interactionTypes.forEach((type: any) => {
+            if(type.directionality === 'INHIBITORY') {
+              countCopy[1] = count[1]++
+            } else {
+              countCopy[0] = count[0]++
+            }
+          })
+        }
+        setCount(countCopy)
+        setChartData({
+          labels: ['Activating', 'Inhibiting', 'N/A'],
+          datasets: [
+            {
+              data: countCopy,
+              backgroundColor: ['#480A77', '#8075FF', '#89E8F1', '#FA198B', '#4BC6B9', '#F0EFF4', '#D1CFE2', '#BAA898'],
+            }
+          ]
+        })
+      })
+    })
+  }, [data])
 
   return (
     <div className="pie-chart-container">
