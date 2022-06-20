1b5468043ece41a484d3aa070c850e0b63056ac9
cjosu <88686663+cjosu@users.noreply.github.com>
2022-06-19 23:43:09 -0400

    Adding filters to interaction table

--- client/src/components/Gene/GeneIntTable/GeneIntTable.tsx
+++ client/src/components/Gene/GeneIntTable/GeneIntTable.tsx
@@ -16,6 +16,15 @@ export const GeneIntTable: React.FC = () => {
 
   const {state} = useContext(GlobalClientContext);
   const [interactionResults, setInteractionResults] = useState<any[]>([]);
+
+  //filter options
+  const [gene, setGene] = useState<any>([]);
+  const [drug, setDrug] = useState<any>([]);
+  const [approvalStatus, setApprovalStatus] = useState<any>([]);
+  const [indication, setIndication] = useState<any>([]);
+  const [intScore, setIntScore] = useState<any>([]);
+  const [queryScore, setQueryScore] = useState<any>([]);
+
   const navigate = useNavigate();
 
   const { data, isError, isLoading } = useGetInteractionsByGenes(state.searchTerms);
@@ -42,21 +51,39 @@ export const GeneIntTable: React.FC = () => {
       dataIndex: ['gene', 'name'],
       render: (text: any, record: any) => (
         <span className="cursor-pointer" onClick={() => navToRecord(record?.gene?.name)}>{record?.gene?.name}</span>
-      )
+      ),
+      filters: gene.map((el: any) => {
+        return {
+          text: el,
+          value: el,
+        }
+      }),
     },
     {
       title: 'Drug',
       dataIndex: ['drug', 'name'],
       render: (text: any, record: any) => (
         <span>{record?.drug?.name}</span>
-      )
+      ),
+      filters: drug.map((el: any) => {
+        return {
+          text: el,
+          value: el,
+        }
+      }),
     },
     {
       title: 'Approval Status',
       dataIndex: ['drug', 'approved'],
       render: (text: any, record: any) => (
         <span>{record?.drug?.approved ? 'Approved' : 'Not Approved'}</span>
-      )
+      ),
+      filters: approvalStatus.map((el: any) => {
+        return {
+          text: el,
+          value: el,
+        }
+      }),
     },
     {
       title: 'Indication',
@@ -72,23 +99,145 @@ export const GeneIntTable: React.FC = () => {
           return <span>{attribute.value}</span>
         })
 
-      }
+      },
+      filters: indication.map((el: any) => {
+        return {
+          text: el,
+          value: el,
+        }
+      }),
     },
     {
       title: 'Interaction Score',
       dataIndex: ['interactionScore'],
       render: (text: any, record: any) => (
         <span>{truncateDecimals(record?.interactionScore, 2)}</span>
-      )
+      ),
+      filters: intScore.map((el: any) => {
+        return {
+          text: el,
+          value: el,
+        }
+      }),
     },
     {
       title: 'Query Score',
       dataIndex: ['queryScore'],
       render: (text: any, record: any) => (
         <span></span>
-      )
+      ),
+      filters: queryScore.map((el: any) => {
+        return {
+          text: el,
+          value: el,
+        }
+      }),
     },
   ]
+
+  const onlyUnique = (value: any, index: any, self: any) => {
+    return self.indexOf(value) === index;
+  }
+
+  useEffect(() => {
+    if (interactionResults){
+      clearFilters();
+    }
+  }, [interactionResults]);
+
+  const clearFilters = () => {
+    let duplicateGene = interactionResults?.map((item: any) => {
+      return item?.gene?.name;
+    })
+    let duplicateDrug = interactionResults?.map((item: any) => {
+      return item?.drug?.name;
+    })
+    let duplicateApprovalStatus = interactionResults?.map((item: any) => {
+      return item?.drug?.approved;
+    })
+    let duplicateIndication = interactionResults?.map((item: any) => {
+      // TODO: this is an array of Drug Class + Drug Indications, find each Drug Indication
+      return item?.drug?.drugAttributes[0]?.name;
+    })
+    let duplicateIntScore = interactionResults?.map((item: any) => {
+      //TODO: make this a range
+      return item?.interactionScore;
+    })
+    let duplicateQueryScore = interactionResults?.map((item: any) => {
+      //TODO: make this query score
+      return item?.interactionScore;
+    })
+
+    // setting state to unique instances of object values
+    setGene(duplicateGene.filter(onlyUnique))
+    setDrug(duplicateDrug.filter(onlyUnique))
+    setApprovalStatus(duplicateApprovalStatus.filter(onlyUnique))
+    setIndication(duplicateIndication.filter(onlyUnique))
+    setIntScore(duplicateIntScore.filter(onlyUnique))
+    setQueryScore(duplicateQueryScore.filter(onlyUnique))
+  }
+
+  enum ColumnType {
+    gene = 1,
+    drug = 2,
+    approvalStatus = 3,
+    indication = 4,
+    intScore = 5,
+    queryScore = 6
+  }
+
+  const columnFilter = (unfilteredData: any, column: ColumnType) => {
+    let duplicates = unfilteredData?.map((item: any) => {
+      switch (column) {
+        case 1:
+          return item?.gene?.name;
+        case 2:
+          return item?.drug?.name;
+        case 3:
+          return item?.drug?.approved;
+        case 4:
+          return item?.drug?.drugAttributes[0]?.name;
+        case 5:
+          return item?.interactionScore;
+        case 6:
+          return item?.interactionScore;
+        default:
+          return null;
+      }
+    })
+    return duplicates.filter(onlyUnique);
+  }
+
+  function onFilterChange(pagination: any, filters: any, sorter: any, extra: any) {
+    for (let prop in filters){
+      // narrow filter options ONLY for columns which have no filtering
+      // this prevents the filtered column from narrowing its own options + not being able to reset
+      if (!filters[prop]){
+        switch (prop) {
+          case "variation.source_label":
+            setGene(columnFilter(extra.currentDataSource, 1));
+            break;
+          case "evidence_level.normalized_evidence_level":
+            setDrug(columnFilter(extra.currentDataSource, 2));
+            break;
+          case "disease.normalized_label":
+            setApprovalStatus(columnFilter(extra.currentDataSource, 3));
+            break;
+          case "therapy.normalized_label":
+            setIndication(columnFilter(extra.currentDataSource, 4));
+            break;
+          case "source":
+            setIntScore(columnFilter(extra.currentDataSource, 5));
+            break;
+          case "clinical_significance":
+            setQueryScore(columnFilter(extra.currentDataSource, 6));
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
 
   // if (isError || isLoading) {
   //   return (
@@ -109,6 +258,7 @@ export const GeneIntTable: React.FC = () => {
         <Table 
           dataSource={interactionResults}
           columns={columns}
+          onChange={onFilterChange}
           rowKey={(record, index) => `${index}`}
           pagination={{ pageSize: 20}}
         />
