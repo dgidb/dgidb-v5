1e534ae991814dafb7f027866624fcdeb42c5a86
cjosu <88686663+cjosu@users.noreply.github.com>
2022-06-27 12:27:17 -0400

    Update FAQ section

--- client/src/pages/About/SubSections/FAQ.tsx
+++ client/src/pages/About/SubSections/FAQ.tsx
@@ -1,10 +1,9 @@
-export const Overview = () => {
+export const FAQ = () => {
 
   return(
-    <div className="about-section-container doc-section">
-      <p>
-
-      </p>
+    <div className="faq-section-container doc-section">
+      <div className="faq-question"> What is the goal of DGIdb? </div>
+      <div className="faq-answer">The goal of DGIdb is to help you annotate your genes of interest with respect to known drug-gene interactions and potential druggability.</div>
     </div>
   )
 }
