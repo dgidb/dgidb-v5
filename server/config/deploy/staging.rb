3b76a19994a6175ba58c6f9dcd75625a68a3bd50
Adam Coffman <acoffman@wustl.edu>
2022-06-28 16:45:52 -0500

    use correct environment in staging

--- server/config/deploy/staging.rb
+++ server/config/deploy/staging.rb
@@ -3,7 +3,7 @@ server "100.21.172.112", user: 'ubuntu', roles: %w{web db app}
 set :branch, 'staging-capistrano'
 set :rbenv_ruby, '3.1.2'
 
-set :rails_env, 'production'
+set :rails_env, 'staging'
 
 set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
