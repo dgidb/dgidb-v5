952b2be9d5aa9c966880f9e645ae81aa91168fb2
cjosu <88686663+cjosu@users.noreply.github.com>
2022-06-29 14:10:12 -0400

    Update section - Known Data Clients

--- client/src/pages/About/SubSections/KnownDataClients.tsx
+++ client/src/pages/About/SubSections/KnownDataClients.tsx
@@ -1,36 +1,38 @@
-export const Overview = () => {
+export const KnownDataClients = () => {
 
   return(
     <div className="about-section-container doc-section">
 
       Data clients are academic and commercial resources that use DGIdb data (for example by integrating DGIdb interactions into their own tool). The current list of DGIdb data clients includes:
 
-      BioGPS
-      CancerGD
-      CancerTracer
-      CANCROX
-      CoDReS
-      CSgator
-      GEMINI - GEnome MINIng
-      Gene4Denovo
-      GeneCards
-      GMIEC - a shiny app
-      Hemap
-      mTCTScan
-      NCG - Network of Cancer Genes
-      NDEx
-      OncoGemini
-      PANDA (Pathway AND Annotation) Explorer
-      Personalized Cancer Therapy
-      TargetDB
-      SL-BioDP
-      SwissMTB
-      theRING
-      TCLP - TRON Cell Line Portal
-      VarCards
-      VAReporter
+    <ul>
+      <li>BioGPS</li>
+      <li>CancerGD</li>
+      <li>CancerTracer</li>
+      <li>CANCROX</li>
+      <li>CoDReS</li>
+      <li>CSgator</li>
+      <li>GEMINI - GEnome MINIng</li>
+      <li>Gene4Denovo</li>
+      <li>GeneCards</li>
+      <li>GMIEC - a shiny app</li>
+      <li>Hemap</li>
+      <li>mTCTScan</li>
+      <li>NCG - Network of Cancer Genes</li>
+      <li>NDEx</li>
+      <li>OncoGemini</li>
+      <li>PANDA (Pathway AND Annotation) Explorer</li>
+      <li>Personalized Cancer Therapy</li>
+      <li>TargetDB</li>
+      <li>SL-BioDP</li>
+      <li>SwissMTB</li>
+      <li>theRING</li>
+      <li>TCLP - TRON Cell Line Portal</li>
+      <li>VarCards</li>
+      <li>VAReporter</li>
+    </ul>
 
-      If your resource is using DGIdb but is missing from this list and you would like us to add it, please contact us at help@dgidb.org.
+      <p>If your resource is using DGIdb but is missing from this list and you would like us to add it, please contact us at help@dgidb.org.</p>
     </div>
   )
 }
