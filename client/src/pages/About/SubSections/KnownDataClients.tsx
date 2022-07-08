d2b93d7b639457f31e71c354de677cabf3258334
cjosu <88686663+cjosu@users.noreply.github.com>
2022-07-08 14:17:09 -0400

    Adding Known Data Clients

--- client/src/pages/About/SubSections/KnownDataClients.tsx
+++ client/src/pages/About/SubSections/KnownDataClients.tsx
@@ -1,35 +1,35 @@
 export const KnownDataClients = () => {
 
   return(
-    <div className="about-section-container doc-section">
+    <div className="known-section-container doc-section">
 
       Data clients are academic and commercial resources that use DGIdb data (for example by integrating DGIdb interactions into their own tool). The current list of DGIdb data clients includes:
 
     <ul>
-      <li>BioGPS</li>
-      <li>CancerGD</li>
-      <li>CancerTracer</li>
-      <li>CANCROX</li>
-      <li>CoDReS</li>
-      <li>CSgator</li>
-      <li>GEMINI - GEnome MINIng</li>
-      <li>Gene4Denovo</li>
-      <li>GeneCards</li>
-      <li>GMIEC - a shiny app</li>
-      <li>Hemap</li>
-      <li>mTCTScan</li>
-      <li>NCG - Network of Cancer Genes</li>
-      <li>NDEx</li>
-      <li>OncoGemini</li>
-      <li>PANDA (Pathway AND Annotation) Explorer</li>
-      <li>Personalized Cancer Therapy</li>
-      <li>TargetDB</li>
-      <li>SL-BioDP</li>
-      <li>SwissMTB</li>
-      <li>theRING</li>
-      <li>TCLP - TRON Cell Line Portal</li>
-      <li>VarCards</li>
-      <li>VAReporter</li>
+      <li><a href="http://biogps.org/#goto=welcome">BioGPS</a></li>
+      <li><a href="http://www.cancergd.org">CancerGD</a></li>
+      <li><a href="http://cailab.labshare.cn/cancertracer/index.html">CancerTracer</a></li>
+      <li><a href="http://cancrox.gmb.bio.br/view/index.php">CANCROX</a></li>
+      <li><a href="http://bioinformatics.cing.ac.cy/codres">CoDReS</a></li>
+      <li><a href="http://csgator.ewha.ac.kr">CSgator</a></li>
+      <li><a href="https://gemini.readthedocs.io/en/latest/index.html">GEMINI - GEnome MINIng</a></li>
+      <li><a href="http://www.genemed.tech/gene4denovo/home">Gene4Denovo</a></li>
+      <li><a href="/">GeneCards</a></li>
+      <li><a href="/">GMIEC - a shiny app</a></li>
+      <li><a href="/">Hemap</a></li>
+      <li><a href="/">mTCTScan</a></li>
+      <li><a href="/">NCG - Network of Cancer Genes</a></li>
+      <li><a href="/">NDEx</a></li>
+      <li><a href="/">OncoGemini</a></li>
+      <li><a href="/">PANDA (Pathway AND Annotation) Explorer</a></li>
+      <li><a href="/">Personalized Cancer Therapy</a></li>
+      <li><a href="/">TargetDB</a></li>
+      <li><a href="/">SL-BioDP</a></li>
+      <li><a href="/">SwissMTB</a></li>
+      <li><a href="/">theRING</a></li>
+      <li><a href="/">TCLP - TRON Cell Line Portal</a></li>
+      <li><a href="/">VarCards</a></li>
+      <li><a href="/">VAReporter</a></li>
     </ul>
 
       <p>If your resource is using DGIdb but is missing from this list and you would like us to add it, please contact us at help@dgidb.org.</p>
