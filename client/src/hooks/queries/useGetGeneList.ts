0a0502923cb8f7fe37b53eaea382210a51e62683
cjosu <88686663+cjosu@users.noreply.github.com>
2022-08-01 11:59:22 -0400

    usegetgenelist comment

--- client/src/hooks/queries/useGetGeneList.ts
+++ client/src/hooks/queries/useGetGeneList.ts
@@ -2,13 +2,13 @@ import { useQuery } from 'react-query';
 import { gql } from 'graphql-request';
 import { graphQLClient } from 'config';
 
-
+//TODO: this works in graphiql but not here? Field 'nodes' doesnt exist on type 'Query'
 const getGeneListQuery = gql`
 query categories($categoryName: [String!]!) {
   nodes {
     name
     id
-    genes ($first: 10) {
+    genes (first:10) {
       pageInfo {
         startCursor
         endCursor
@@ -17,7 +17,7 @@ query categories($categoryName: [String!]!) {
       nodes {
         name
         longName
-        geneCategoriesWithSources ($categoryName: "ENZYME") {
+        geneCategoriesWithSources (categoryName: "ENZYME") {
           sourceNames
         }
       }
