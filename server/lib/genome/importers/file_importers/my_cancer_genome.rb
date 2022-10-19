bf77a2d43b2b3239d3562b090ac2c76b079bca22
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-10-19 17:12:48 -0400

    Standardize ID names

--- server/lib/genome/importers/file_importers/my_cancer_genome.rb
+++ server/lib/genome/importers/file_importers/my_cancer_genome.rb
@@ -33,7 +33,7 @@ module Genome; module Importers; module FileImporters; module MyCancerGenome;
     def create_interaction_claims
       CSV.foreach(file_path, headers: true, col_sep: "\t") do |row|
         gene_claim = create_gene_claim(row['Gene Symbol'], 'MyCancerGenome Gene Symbol')
-        create_gene_claim_alias(gene_claim, "ncbigene:#{row['Entrez Gene Id']}", 'NCBI Gene Id')
+        create_gene_claim_alias(gene_claim, "ncbigene:#{row['Entrez Gene Id']}", 'NCBI Gene ID')
         create_gene_claim_alias(gene_claim, row['Gene Symbol'], 'MyCancerGenome Gene Symbol')
         create_gene_claim_alias(gene_claim, row['Reported Gene Name'], 'MyCancerGenome Reported Gene Name')
 
