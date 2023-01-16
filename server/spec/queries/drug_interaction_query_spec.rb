99e2e407740b4b2e93ef5700e0ec4061289c722d
James Stevenson <james.stevenson@nationwidechildrens.org>
2023-01-16 13:00:45 -0500

    Add WIP drug query

---
+++ server/spec/queries/drug_interaction_query_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe 'Drug Interaction Query', type: :graphql do
+  before(:example) do
+    @drug = build(:drug, approved: true)
+    @cat = create(:gene_claim_category)
+    @gene = create(:gene, gene_categories: [@cat])
+    @int_type = create(:interaction_claim_type)
+    @int = create(:interaction, drug: @drug, gene: @gene, interaction_types: [@int_type])
+  end
+
+  # TODO share query with frontend?
+  let :query do
+    <<-GRAPHQL
+    query drugs($names: [String!]!) {
+      drugs(name: $names) {
+        interactions {
+          gene {
+            name
+            geneCategories {
+              name
+            }
+          }
+          drug {
+            name
+            approved
+          }
+          interactionScore
+          interactionTypes {
+            type
+            directionality
+          }
+        }
+      }
+    }
+    GRAPHQL
+  end
+
+  it 'should execute getInteractionsByDrugsQuery correctly' do
+    result = execute_graphql(query, variables: { names: [@drug.name] })
+    drugs = result['data']['drugs']
+    expect(drugs.size).to eq 1
+
+    interactions = drugs[0]['interactions']
+    expect(interactions.size).to eq 1
+    interaction = interactions[0]
+
+    gene = interaction['gene']
+    expect(gene['name']).to eq @gene.name
+    expect(gene['geneCategories'].size).to eq 1
+    expect(gene['geneCategories'][0]['name']).to eq 'CLINICALLY ACTIONABLE'
+
+    drug = interaction['drug']
+    expect(drug['name']).to eq @drug.name
+    # expect(drug['approved']).to be_true  # TODO fails
+
+    expect(interaction['interactionScore']).to eq @int.score
+    expect(interaction['interactionTypes'].size).to eq 1
+    expect(interaction['interactionTypes'][0]['type']).to eq 'ligand'
+    expect(interaction['interactionTypes'][0]['directionality']).to be_nil
+  end
+end
