2a60ab860bad29b52147d926bdd0db38f31e5419
James Stevenson <james.stevenson@nationwidechildrens.org>
2023-01-20 17:58:47 -0500

    refactor: use resolver method for aliases (#259)
    avoid namespace collision/resolve obnoxious warnings

--- server/app/graphql/types/gene_claim_alias_type.rb
+++ server/app/graphql/types/gene_claim_alias_type.rb
@@ -2,10 +2,14 @@ module Types
   class GeneClaimAliasType < Types::BaseObject
     field :id, ID, null: false
     field :gene_claim_id, ID, null: false
-    field :alias, String, null: false
+    field :alias, String, null: false, resolver_method: :resolve_alias
     field :nomenclature, String, null: false
 
     field :gene_claim, Types::GeneClaimType, null: false
+
+    def resolve_alias
+      object.alias
+    end
 
     def gene_claim
       Loaders::RecordLoader.for(GeneClaim).load(object.gene_claim_id)
