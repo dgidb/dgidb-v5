bcf3991064c02f546105881fd9635643549827bd
James Stevenson <james.stevenson@nationwidechildrens.org>
2023-01-19 19:48:46 -0500

    udpate tests

--- server/spec/queries/drug_query_spec.rb
+++ server/spec/queries/drug_query_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Genes Query', type: :graphql do
+RSpec.describe 'Drugs Query', type: :graphql do
   before(:example) do
     @drug_alias = create(:drug_alias)
     @drug_attr = create(:drug_attribute)
@@ -9,29 +9,28 @@ RSpec.describe 'Genes Query', type: :graphql do
 
   let :query do
     <<-GRAPHQL
-    query drugs($names: [String!]!) {
-      drugs(names: $names) {
-        nodes {
-          drugAliases {
-            alias
-          }
-          drugAttributes {
-            id
-            name
-            value
-          }
-
+    query drug($name: String!) {
+      drug(name: $name) {
+        conceptId
+        drugAliases {
+          alias
         }
+        drugAttributes {
+          id
+          name
+          value
+        }
+
       }
     }
     GRAPHQL
   end
 
   it 'should execute getDrugRecordQuery correctly' do
-    result = execute_graphql(query, variables: { names: [@drug.name] })
-    expect(result['data']['drugs']['nodes'].size).to eq 1
+    result = execute_graphql(query, variables: { name: @drug.name })
+    drug = result['data']['drug']
 
-    drug = result['data']['drugs']['nodes'][0]
+    expect(drug['conceptId']).to eq @drug.concept_id
 
     expect(drug['drugAliases'].size).to eq 1
     expect(drug['drugAliases'][0]['alias']).to eq @drug_alias.alias
@@ -42,4 +41,3 @@ RSpec.describe 'Genes Query', type: :graphql do
     expect(drug['drugAttributes'][0]['value']).to eq @drug_attr.value
   end
 end
-
