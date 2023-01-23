8460e6b97055c350927c0c2e8fe8cb8f5f0182a4
James Stevenson <james.stevenson@nationwidechildrens.org>
2023-01-23 09:59:09 -0500

    format

--- client/src/components/Browse/Categories/BrowseCategories.tsx
+++ client/src/components/Browse/Categories/BrowseCategories.tsx
@@ -1,24 +1,24 @@
 // hooks/dependencies
-import React, {useState, useContext, useEffect} from 'react';
-import { useGetInteractionsByGenes} from 'hooks/queries/useGetInteractions';
-import { useGetGeneRecord } from 'hooks/queries/useGetGeneRecord';
-import { useGetDruggableSources } from 'hooks/queries/useGetDruggableSources';
-import { useGetCategoriesBySource } from 'hooks/queries/useGetCategories';
-import { Collapse } from 'antd';
+import React, { useState, useContext, useEffect } from "react";
+import { useGetInteractionsByGenes } from "hooks/queries/useGetInteractions";
+import { useGetGeneRecord } from "hooks/queries/useGetGeneRecord";
+import { useGetDruggableSources } from "hooks/queries/useGetDruggableSources";
+import { useGetCategoriesBySource } from "hooks/queries/useGetCategories";
+import { Collapse } from "antd";
 
 // components
-import { GeneListTable } from 'components/Browse/Categories/GeneListTable';
+import { GeneListTable } from "components/Browse/Categories/GeneListTable";
 
 // styles
-import './BrowseCategories.scss';
-import { Checkbox } from 'antd';
+import "./BrowseCategories.scss";
+import { Checkbox } from "antd";
 
 const CheckboxGroup = Checkbox.Group;
 const { Panel } = Collapse;
 
-  interface Categories {
-    [key: string]: number
-  }
+interface Categories {
+  [key: string]: number;
+}
 
 export const BrowseCategories: React.FC = () => {
   const [plainOptions, setPlainOptions] = useState([]);
@@ -27,106 +27,112 @@ export const BrowseCategories: React.FC = () => {
   const [indeterminate, setIndeterminate] = useState(true);
   const [checkAll, setCheckAll] = useState(false);
 
-  const [allCategories, setAllCategories] = useState<any>([])
-  const [renderedCategories, setRenderedCategories] = useState<any>([])
+  const [allCategories, setAllCategories] = useState<any>([]);
+  const [renderedCategories, setRenderedCategories] = useState<any>([]);
 
+  const { data } = useGetDruggableSources("POTENTIALLY_DRUGGABLE");
 
-  const { data } = useGetDruggableSources("POTENTIALLY_DRUGGABLE")
+  useEffect(() => {
+    if (data?.sources?.nodes) {
+      let nodes = data?.sources?.nodes;
+      let sources: any = [];
 
-    useEffect(() => {
-      if (data?.sources?.nodes){
-        let nodes = data?.sources?.nodes
-        let sources: any = [];
+      nodes.forEach((node: any) => {
+        sources.push(node.sourceDbName);
+      });
 
-        nodes.forEach((node: any) => {
-          sources.push(node.sourceDbName)
-        })
+      setPlainOptions(sources);
 
-        setPlainOptions(sources);
+      let allCategoriesObj: Categories = {};
 
-        let allCategoriesObj: Categories = {}
+      nodes.forEach((node: any) => {
+        node.categoriesInSource.forEach((cat: any) => {
+          let key = cat.name;
+          allCategoriesObj[key] = 0;
+        });
+      });
 
-        nodes.forEach((node: any) => {
-          node.categoriesInSource.forEach((cat: any) => {
-            let key = cat.name
-            allCategoriesObj[key] = 0;
-          })
-        })
+      setAllCategories(allCategoriesObj);
+    }
+  }, [data]);
 
-        setAllCategories(allCategoriesObj);
+  useEffect(() => {
+    if (plainOptions) {
+      setCheckedList(plainOptions);
+    }
+  }, [plainOptions]);
+
+  useEffect(() => {
+    let allCategoriesCopy: Categories = {};
+
+    // return each node where where checked item is present
+    data?.sources?.nodes?.forEach((src: any) => {
+      let includes: any = checkedList.includes(src.sourceDbName);
+      if (includes) {
+        let cats: any = src?.categoriesInSource;
+        cats?.forEach((cat: any) => {
+          if (typeof allCategoriesCopy[cat.name] === "number") {
+            allCategoriesCopy[cat.name] += cat.geneCount;
+          } else {
+            allCategoriesCopy[cat.name] = cat.geneCount;
+          }
+        });
       }
-    }, [data])
+    });
 
-    useEffect(() => {
-      if (plainOptions) {
-        setCheckedList(plainOptions);
-      }
-    }, [plainOptions])
+    let categoriesArray = [];
 
-    useEffect(() => {
-      let allCategoriesCopy: Categories = {};
+    for (const key in allCategoriesCopy) {
+      categoriesArray.push({ name: key, geneCount: allCategoriesCopy[key] });
+    }
 
-      // return each node where where checked item is present
-      data?.sources?.nodes?.forEach((src: any) => {
-        let includes: any = checkedList.includes(src.sourceDbName)
-        if (includes) {
-          let cats: any = src?.categoriesInSource;
-          cats?.forEach((cat: any) => {
-            if(typeof allCategoriesCopy[cat.name] === 'number') {
-              allCategoriesCopy[cat.name] += cat.geneCount
-            } else {
-              allCategoriesCopy[cat.name] = cat.geneCount
-            }
-          })
-        }
-      })
+    setRenderedCategories(categoriesArray);
+  }, [checkedList]);
 
-      let categoriesArray = [];
+  const onChange = (list: any) => {
+    setCheckedList(list);
+    setIndeterminate(!!list.length && list.length < plainOptions.length);
+    setCheckAll(list.length === plainOptions.length);
+  };
 
-      for (const key in allCategoriesCopy) {
-        categoriesArray.push({name: key, geneCount: allCategoriesCopy[key]})
-      }
+  const onCheckAllChange = (e: any) => {
+    setCheckedList(e.target.checked ? plainOptions : []);
+    setIndeterminate(false);
+    setCheckAll(e.target.checked);
+  };
 
-      setRenderedCategories(categoriesArray);
-    }, [checkedList])
+  return (
+    <div className="browse-categories-container">
+      <div className="source-checklist">
+        <CheckboxGroup
+          options={plainOptions}
+          value={checkedList}
+          onChange={onChange}
+        />
+        <Checkbox
+          indeterminate={indeterminate}
+          onChange={onCheckAllChange}
+          checked={checkAll}
+        >
+          Select/Deselect All
+        </Checkbox>
+      </div>
 
-    const onChange = (list: any) => {
-      setCheckedList(list);
-      setIndeterminate(!!list.length && list.length < plainOptions.length);
-      setCheckAll(list.length === plainOptions.length);
-    };
-
-    const onCheckAllChange = (e: any) => {
-      setCheckedList(e.target.checked ? plainOptions : []);
-      setIndeterminate(false);
-      setCheckAll(e.target.checked);
-    };
-
-    return (
-      <div className="browse-categories-container">
-        <div className="source-checklist">
-         <CheckboxGroup options={plainOptions} value={checkedList} onChange={onChange} />
-          <Checkbox indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll}>
-            Select/Deselect All
-          </Checkbox>
-        </div>
-
-        <div className="category-list">
-           <Collapse accordion>
+      <div className="category-list">
+        <Collapse accordion>
           {renderedCategories?.map((cat: any, index: number) => {
-            if(cat.geneCount) {
+            if (cat.geneCount) {
               return (
                 <Panel header={`${cat.name} ${cat.geneCount}`} key={index}>
                   <GeneListTable />
-               </Panel>
-              )
+                </Panel>
+              );
             } else {
-              return null
+              return null;
             }
           })}
-          </Collapse>
-
-        </div>
+        </Collapse>
       </div>
-    )
-}
+    </div>
+  );
+};
