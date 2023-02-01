6aeda3604d1a210849e34d56920617c7e7e54a0b
mcannon068nw <Matthew.Cannon2@nationwidechildrens.org>
2023-02-01 12:16:28 -0500

    updated current contributors, added float styling for two column layout

--- client/src/pages/About/SubSections/Contact.tsx
+++ client/src/pages/About/SubSections/Contact.tsx
@@ -2,46 +2,71 @@ export const Contact = () => {
 
   return(
     <div className="contact-section-container doc-section">
-      <p>DGIdb was developed at The McDonnell Genome Institute, Washington University School of Medicine. If you have a source of information related to the druggable genome you would like us to incorporate, please contact us at help@dgidb.org.</p>
-      <p>Citation: Integration of the Drug–Gene Interaction Database (DGIdb 4.0) with open crowdsource efforts. Freshour SL, Kiwala S, Cotto KC, Coffman AC, McMichael JF, Song JJ, Griffith M, Griffith OL, Wagner AH. Nucleic Acids Research. 2021 Jan 8. doi:https://doi.org/10.1093/nar/gkaa1084.</p>
+      <div>
+        <p>DGIdb was developed at The McDonnell Genome Institute, Washington University School of Medicine. If you have a source of information related to the druggable genome you would like us to incorporate, please contact us at help@dgidb.org.</p>
+        <p>Citation: Integration of the Drug–Gene Interaction Database (DGIdb 4.0) with open crowdsource efforts. Freshour SL, Kiwala S, Cotto KC, Coffman AC, McMichael JF, Song JJ, Griffith M, Griffith OL, Wagner AH. Nucleic Acids Research. 2021 Jan 8. doi:https://doi.org/10.1093/nar/gkaa1084.</p>
+        <h3>Current Contributors</h3>
+      </div>
 
-      <h3>Current Contributors</h3>
-      <h4>The McDonnell Genome Institute</h4>
-      <p><a href="http://genome.wustl.edu/">http://genome.wustl.edu/</a><i ></i></p>
+      <div className="contact-section-container left-section">
+        <h4>The McDonnell Genome Institute</h4>
+        <p><a href="http://genome.wustl.edu/">http://genome.wustl.edu/</a></p>
 
-      <h4>Malachi Griffith</h4>
-      <p>Creator<br/>
-      <a href="https://griffithlab.org">Faculty profile</a><i ></i><br/>
-      <a href="mailto:mgriffit@wustl.edu">Email</a></p>
-      <h4>Obi Griffith</h4>
-      <p>Creator<br/>
-      <a href="https://griffithlab.org">Faculty profile</a><i ></i><br/>
-      <a href="mailto:obigriffith@wustl.edu">Email</a></p>
-      <h4>Alex Wagner</h4>
-      <p>Creator, developer<br/>
-      <a href="http://alexwagner.info">Academic profile</a><i ></i><br/>
-      <a href="mailto:Alex.Wagner@nationwidechildrens.org">Email</a></p>
-      <h4>Susanna Kiwala</h4>
-      <p>Software developer<br/>
-      <a href="https://github.com/susannasiebert">Github profile</a><i ></i><br/>
-      <a href="mailto:susanna.kiwala@wustl.edu">Email</a></p>
-      <h4>Kelsy Cotto</h4>
-      <p>Curator, developer<br/>
-      <a href="https://www.linkedin.com/in/kelsy-cotto-94b9018b/">LinkedIn profile</a><i ></i><br/>
-      <a href="mailto:kelsy.cotto@wustl.edu">Email</a></p>
-      <h4>Sharon Freshour</h4>
-      <p>Curator, developer<br/>
-      <a href="https://www.linkedin.com/in/sharonfreshour/">LinkedIn profile</a><i ></i><br/>
-      <a href="mailto:sharonfreshour@wustl.edu">Email</a></p>
-      <h4>Adam Coffman</h4>
-      <p>Software developer
-      <br/><a href="https://github.com/acoffman">Github profile</a><i ></i><br/>
-      <a href="mailto:acoffman@wustl.edu">Email</a></p>
-      <h4>Josh McMichael</h4>
-      <p>User experience (UX) developer<br/>
-      <a href="https://github.com/jmcmichael">Github profile</a><i ></i><br/>
-      <a href="mailto:jmcmicha@wustl.edu">Email</a></p>
+        <h4>Malachi Griffith</h4>
+        <p>Creator<br/>
+        <a href="https://griffithlab.org">Faculty profile</a><br/>
+        <a href="mailto:mgriffit@wustl.edu">Email</a></p>
+        <h4>Obi Griffith</h4>
+        <p>Creator<br/>
+        <a href="https://griffithlab.org">Faculty profile</a><br/>
+        <a href="mailto:obigriffith@wustl.edu">Email</a></p>
+        <h4>Susanna Kiwala</h4>
+        <p>Software developer<br/>
+        <a href="https://github.com/susannasiebert">Github profile</a><br/>
+        <a href="mailto:susanna.kiwala@wustl.edu">Email</a></p>
+        <h4>Kelsy Cotto</h4>
+        <p>Curator, developer<br/>
+        <a href="https://www.linkedin.com/in/kelsy-cotto-94b9018b/">LinkedIn profile</a><br/>
+        <a href="mailto:kelsy.cotto@wustl.edu">Email</a></p>
+        <h4>Sharon Freshour</h4>
+        <p>Curator, developer<br/>
+        <a href="https://www.linkedin.com/in/sharonfreshour/">LinkedIn profile</a><br/>
+        <a href="mailto:sharonfreshour@wustl.edu">Email</a></p>
+        <h4>Adam Coffman</h4>
+        <p>Software developer
+        <br/><a href="https://github.com/acoffman">Github profile</a><br/>
+        <a href="mailto:acoffman@wustl.edu">Email</a></p>
+        <h4>Josh McMichael</h4>
+        <p>User experience (UX) developer<br/>
+        <a href="https://github.com/jmcmichael">Github profile</a><br/>
+        <a href="mailto:jmcmicha@wustl.edu">Email</a></p>
+      </div>
 
+      <div className="contact-section-container right-section">
+        <h4>The Institute for Genomic Medicine</h4>
+        <p><a href="https://www.nationwidechildrens.org/specialties/institute-for-genomic-medicine">https://www.nationwidechildrens.org/specialties/institute-for-genomic-medicine</a></p>
+        <h4>Alex Wagner</h4>
+        <p>Creator, developer<br/>
+        <a href="http://alexwagner.info">Academic profile</a><br/>
+        <a href="mailto:Alex.Wagner@nationwidechildrens.org">Email</a></p>
+        <h4>Matthew Cannon</h4>
+        <p>Developer<br/>
+        <a href="">LinkedIn Profile</a><br/>
+        <a href="mailto:Matthew.Cannon2@nationwidechildrens.org">Email</a></p>
+        <h4>Katie Stahl</h4>
+        <p>Developer<br/>
+        <a href="https://github.com/katiestahl">GitHub</a><br/>
+        <a href="https://www.linkedin.com/in/katie-stahl-05b81a126/">LinkedIn</a><br/>
+        <a href="mailto:Kathryn.Stahl@nationwidechildrens.org">Email</a></p>
+        <h4>James Stevenson</h4>
+        <p>Developer<br/>
+        <a href="https://www.linkedin.com/in/james-stevenson-00999482/">LinkedIn</a><br/>
+        <a href="https://jsstevenson.github.io/">Github.io</a><br/>
+        <a href="mailto:James.Stevenson@nationwidechildrens.org">Email</a></p>
+      </div>
+      <div className="contact-section-container spacing"></div>
+
+      <div className="contact-session-container">
       <h3>Acknowledgements</h3>
 
         <h4>Conceptual designers / mentors</h4>
@@ -61,7 +86,7 @@ export const Contact = () => {
 
         <h4>Miscellaneous contributions</h4>
         <p>Kilannin Krysiak, Zachary Skidmore, Robert Lesurf, Lee Trani, Jonathan Song</p>
-
+      </div>
 
 
     </div>
