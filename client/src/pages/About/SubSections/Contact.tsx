b06471b7b8d1e27e80a826bad9be7989a9122af3
mcannon068nw <Matthew.Cannon2@nationwidechildrens.org>
2023-02-01 11:17:46 -0500

    added pre-existing contacts/acknowledgements with basic format

--- client/src/pages/About/SubSections/Contact.tsx
+++ client/src/pages/About/SubSections/Contact.tsx
@@ -6,6 +6,65 @@ export const Contact = () => {
       <p>Citation: Integration of the Drug–Gene Interaction Database (DGIdb 4.0) with open crowdsource efforts. Freshour SL, Kiwala S, Cotto KC, Coffman AC, McMichael JF, Song JJ, Griffith M, Griffith OL, Wagner AH. Nucleic Acids Research. 2021 Jan 8. doi:https://doi.org/10.1093/nar/gkaa1084.</p>
 
       <h3>Current Contributors</h3>
+      <h4>The McDonnell Genome Institute</h4>
+      <p><a href="http://genome.wustl.edu/">http://genome.wustl.edu/</a><i ></i></p>
+
+      <h4>Malachi Griffith</h4>
+      <p>Creator<br/>
+      <a href="https://griffithlab.org">Faculty profile</a><i ></i><br/>
+      <a href="mailto:mgriffit@wustl.edu">Email</a></p>
+      <h4>Obi Griffith</h4>
+      <p>Creator<br/>
+      <a href="https://griffithlab.org">Faculty profile</a><i ></i><br/>
+      <a href="mailto:obigriffith@wustl.edu">Email</a></p>
+      <h4>Alex Wagner</h4>
+      <p>Creator, developer<br/>
+      <a href="http://alexwagner.info">Academic profile</a><i ></i><br/>
+      <a href="mailto:Alex.Wagner@nationwidechildrens.org">Email</a></p>
+      <h4>Susanna Kiwala</h4>
+      <p>Software developer<br/>
+      <a href="https://github.com/susannasiebert">Github profile</a><i ></i><br/>
+      <a href="mailto:susanna.kiwala@wustl.edu">Email</a></p>
+      <h4>Kelsy Cotto</h4>
+      <p>Curator, developer<br/>
+      <a href="https://www.linkedin.com/in/kelsy-cotto-94b9018b/">LinkedIn profile</a><i ></i><br/>
+      <a href="mailto:kelsy.cotto@wustl.edu">Email</a></p>
+      <h4>Sharon Freshour</h4>
+      <p>Curator, developer<br/>
+      <a href="https://www.linkedin.com/in/sharonfreshour/">LinkedIn profile</a><i ></i><br/>
+      <a href="mailto:sharonfreshour@wustl.edu">Email</a></p>
+      <h4>Adam Coffman</h4>
+      <p>Software developer
+      <br/><a href="https://github.com/acoffman">Github profile</a><i ></i><br/>
+      <a href="mailto:acoffman@wustl.edu">Email</a></p>
+      <h4>Josh McMichael</h4>
+      <p>User experience (UX) developer<br/>
+      <a href="https://github.com/jmcmichael">Github profile</a><i ></i><br/>
+      <a href="mailto:jmcmicha@wustl.edu">Email</a></p>
+
+      <h3>Acknowledgements</h3>
+
+        <h4>Conceptual designers / mentors</h4>
+        <p>Elaine Mardis, Rick Wilson</p>
+
+        <h4>Additional software developers</h4>
+        <p>Indraniel Das, James Koval, Nicholas Spies, Avinash Ramu, Jim Eldred, Yang-Yang Feng, Jim Weible, Scott Smith, Ben Ainscough</p>
+
+        <h4>Beta testers</h4>
+        <p>David Larson, Jason Walker, Chris Miller, Ron Bose, Runjun Kumar</p>
+
+        <h4>Expert curators</h4>
+        <p>Malachi Griffith, Obi Griffith, Alex Wagner, Janakiraman Subramanian, Nicholas Spies, Jaclyn Boozalis, Deng Pan, Kelsy Cotto, Sharon Freshour</p>
+
+        <h4>Additional conceptual contributors</h4>
+        <p>Timothy J. Ley, Li Ding, David J. Dooling, Ramaswamy Govindan</p>
+
+        <h4>Miscellaneous contributions</h4>
+        <p>Kilannin Krysiak, Zachary Skidmore, Robert Lesurf, Lee Trani, Jonathan Song</p>
+
+
+
     </div>
+
   )
 }
