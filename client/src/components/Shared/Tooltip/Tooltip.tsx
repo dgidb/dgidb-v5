1724a153eb6c7ef7c01074728a8390a69107bb55
mcannon068nw <Matthew.Cannon2@nationwidechildrens.org>
2023-02-01 14:14:28 -0500

    basic working Tooltip

---
+++ client/src/components/Shared/Tooltip/Tooltip.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+
+const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: '#f5f5f9',
+      color: 'rgba(0, 0, 0, 0.87)',
+      maxWidth: 220,
+      fontSize: theme.typography.pxToRem(12),
+      border: '1px solid #dadde9',
+    },
+  }));
+
+  export default function CoolTooltips() {
+    return (
+      <div>
+        <HtmlTooltip
+          title={
+            <React.Fragment>
+              <Typography color="inherit">Tooltip with HTML</Typography>
+              <em>{"And here's"}</em> <b>{'some'}</b> <u>{'amazing content'}</u>.{' '}
+              {"It's very engaging. Right?"}
+            </React.Fragment>
+          }
+        >
+          <Button>HTML</Button>
+        </HtmlTooltip>
+      </div>
+    );
+  }
