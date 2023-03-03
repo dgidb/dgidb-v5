a1d4b3a166676333adca919de458ff92ce3b8b30
Adam Coffman <acoffman@wustl.edu>
2023-03-03 16:06:16 -0600

    hook up download route

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post "/api/graphql", to: "graphql#execute"
+  post "/api/graphql/download/:table_name", to: "table_download#download_table"
 
   mount GraphiQL::Rails::Engine, at: "/api/graphiql", graphql_path: "/api/graphql"
 end
