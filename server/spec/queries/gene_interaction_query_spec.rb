720d1c6f494687fe81e6c6a6c14f1c85f6f67e38
James Stevenson <james.stevenson@nationwidechildrens.org>
2023-03-06 10:11:28 -0500

    tmp

--- server/spec/queries/gene_interaction_query_spec.rb
+++ server/spec/queries/gene_interaction_query_spec.rb
@@ -100,24 +100,5 @@ RSpec.describe 'Gene Interaction Query', type: :graphql do
 
     drug = interaction['drug']
     expect(drug['name']).to eq @drug.name
-    # expect(drug['approved']).to be true
-    expect(drug['drugApprovalRatings'].size).to eq 1
-    expect(drug['drugApprovalRatings'][0]['rating']).to eq @drug_appr_rating.rating
-    expect(drug['drugAttributes'].size).to eq 1
-    expect(drug['drugAttributes'][0]['name']).to eq @drug_attr.name
-    expect(drug['drugAttributes'][0]['value']).to eq @drug_attr.value
-
-    expect(interaction['gene']['name']).to eq @gene.name
-
-    expect(interaction['interactionScore']).to eq @int.score
-    expect(interaction['interactionTypes'].size).to eq 1
-    expect(interaction['interactionTypes'][0]['type']).to eq @int_type.type
-    expect(interaction['interactionTypes'][0]['directionality']).to eq @int_type.directionality
-    expect(interaction['publications'].size).to eq 1
-    expect(interaction['publications'][0]['pmid']).to eq @pub.pmid
-
-    expect(interaction['sources'].size).to eq 1
-    expect(interaction['sources'][0]['id']).to eq @src.id
-    expect(interaction['sources'][0]['fullName']).to eq @src.full_name
   end
 end
