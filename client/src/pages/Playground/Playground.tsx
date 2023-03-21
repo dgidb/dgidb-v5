71996f38b41c70c0fc61b46de83d367e4b63fff1
mcannon068nw <Matthew.Cannon2@nationwidechildrens.org>
2023-03-21 13:52:20 -0400

    spruce up some scss, modify queries

--- client/src/pages/Playground/Playground.tsx
+++ client/src/pages/Playground/Playground.tsx
@@ -23,53 +23,101 @@ const defaultStyle = {
 // queries
 const query1 = `
 {
-  drugs(name: ["DOVITINIB"]) {
-    interactions {
-      gene {
-        name
-      }
-      drug {
-        name
-      }
-      interactionScore
-      interactionTypes {
-        type
-        directionality
+  drugs(names: ["DOVITINIB"]) {
+    nodes {
+      interactions {
+        gene {
+          name
+          conceptId
+          longName
+        }
+        interactionScore
+        interactionTypes {
+          type
+          directionality
+        }
+        publications {
+          pmid
+        }
+        sources {
+          sourceDbName
+        }
       }
     }
   }
-}`;
+}
+`;
 
 const query2 = `
 {
-  genes(name: ["INSR"]) {
-    interactions {
-      gene {
-        name
-      }
-      drug {
-        name
-      }
-      interactionScore
-      interactionTypes {
-        type
-        directionality
+  genes(names: ["BRAF"]) {
+    nodes {
+      interactions {
+        drug {
+          name
+          conceptId
+        }
+        interactionScore
+        interactionTypes {
+          type
+          directionality
+        }
+        publications {
+          pmid
+        }
+        sources {
+          sourceDbName
+        }
       }
     }
   }
-}`;
+}
+`;
 
 const query3 = `
 {
-  drugs(name: ["DOVITINIB"]) {
-    name
-    id
-    conceptId
-    approved
-    immunotherapy
-    antiNeoplastic
+  drugs(names: ["IMATINIB"]) {
+    nodes {
+      name
+      conceptId
+      approved
+      immunotherapy
+      antiNeoplastic
+      drugAttributes {
+        name
+        value
+      }
+      drugApprovalRatings {
+        rating
+        source {
+          sourceDbName
+          sourceTrustLevel {
+            level
+          }
+        }
+      }
+      drugApplications {
+        appNo
+      }
+    }
   }
-}`;
+}
+`;
+
+const query4 = `
+{
+  genes(names: ["BRAF"]) {
+    nodes {
+      longName
+      conceptId
+      geneCategoriesWithSources {
+        name
+        sourceNames
+      }
+    }
+  }
+}
+`
 
 const fetcher = createGraphiQLFetcher({ url: 'http://localhost:3000/api/graphql'})
 
@@ -78,18 +126,27 @@ export const Playground = () => {
     <div className="playground-page-container" >
       <div className="collapse-group">
         <Collapse accordion style={defaultStyle}>
-          <Panel header="Example Query 1" key="1" style={defaultStyle}>
-            <Button onClick={() => setQuery(query1)} style={buttonStyle}>Run Query</Button>
+          <Panel header="Find Interactions (by Drug)" key="1" style={defaultStyle}>
+            Identify drug-gene interactions across 40 aggregate sources per <i>n</i> drug inputs. <br/><br/>
+            <Button onClick={() => setQuery(query1)} style={buttonStyle}>Copy Query to Clipboard</Button>
             <pre><code>{query1}</code></pre>
           </Panel>
-          <Panel header="Example Query 2" key="2" style={defaultStyle}>
-            <Button onClick={() => setQuery(query2)} style={buttonStyle}>Run Query</Button>
+          <Panel header="Find Interactions (by Gene)" key="2" style={defaultStyle}>
+          Identify drug-gene interactions across 40 aggregate sources per <i>n</i> gene inputs. <br/><br/>
+            <Button onClick={() => setQuery(query2)} style={buttonStyle}>Copy Query to Clipboard</Button>
             <pre><code>{query2}</code></pre>
           </Panel>
-          <Panel header="Example Query 3" key="3" style={defaultStyle}>
-            <Button onClick={() => setQuery(query3)} style={buttonStyle}>Run Query</Button>
+          <Panel header="Drug Attributes and Approval Enums" key="3" style={defaultStyle}>
+          Identify drug attributes, approval values, and active ANDA/NDA applications per <i>n</i> drug inputs <br/> <br/>
+            <Button onClick={() => setQuery(query3)} style={buttonStyle}>Copy Query to Clipboard</Button>
             <pre><code>{query3}</code></pre>
           </Panel>
+          <Panel header="Gene Category Annotations" key="4" style={defaultStyle}>
+          Identify annotations for druggability and clinical actionability per <i>n</i> gene inputs <br/> <br/>
+            <Button onClick={() => setQuery(query3)} style={buttonStyle}>Copy Query to Clipboard</Button>
+            <pre><code>{query3}</code></pre>
+          </Panel>
+
         </Collapse>
       </div>
       <div className="main">
