91c3a55b0ade8ae81f302f9dd657de5674c81657
mcannon068nw <Matthew.Cannon2@nationwidechildrens.org>
2023-03-21 09:07:33 -0400

    hyperlink sources: modified hook, added links

--- server/spec/queries/sources_queries_spec.rb
+++ server/spec/queries/sources_queries_spec.rb
@@ -26,6 +26,7 @@ RSpec.describe 'Sources queries', type: :graphql do
         }
         nodes {
           sourceDbName
+          baseUrl
           drugClaimsCount
           drugClaimsInGroupsCount
           citation
@@ -46,6 +47,7 @@ RSpec.describe 'Sources queries', type: :graphql do
     expect(result['data']['sources']['nodes'].size).to eq 1
     source = result['data']['sources']['nodes'][0]
     expect(source['sourceDbName']).to eq @src.source_db_name
+    expect(source['baseUrl']).to eq @src.base_url
 
     expect(source['drugClaimsCount']).to eq 1
     expect(source['drugClaimsInGroupsCount']).to eq 1
