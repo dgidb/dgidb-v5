2d3a2f9a903356c02ec3faaf69659cfe5fed7b97
Kelsy Cotto <kelsy.cotto@wustl.edu>
2023-03-23 15:38:12 -0500

    make stylistic changes to docs

--- client/src/pages/About/SubSections/KnownDataClients.tsx
+++ client/src/pages/About/SubSections/KnownDataClients.tsx
@@ -32,7 +32,7 @@ export const KnownDataClients = () => {
       <li><a href="http://rnd.cgu.edu.tw/vareporter/">VAReporter</a></li>
     </ul>
 
-      <p>If your resource is using DGIdb but is missing from this list and you would like us to add it, please contact us at help@dgidb.org.</p>
+      <p>If your resource is using DGIdb but is missing from this list and you would like us to add it, please contact us at <a href="mailto:help@dgidb.org">help@dgidb.org.</a></p>
     </div>
   )
 }
