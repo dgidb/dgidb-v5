8a4a9babb7f2e2253886815f605d70120d672e55
James Stevenson <james.stevenson@nationwidechildrens.org>
2023-06-02 13:45:09 -0400

    format

--- client/src/utils/generateXrefLink.tsx
+++ client/src/utils/generateXrefLink.tsx
@@ -4,18 +4,33 @@ import { ResultTypes } from 'types/types';
 const drugPatterns: [RegExp, string][] = [
   [/^PUBCHEM.COMPOUND:(.+)/, 'https://pubchem.ncbi.nlm.nih.gov/compound/'],
   [/^CHEMBL:(.+)/i, 'https://www.ebi.ac.uk/chembl/compound_report_card/'],
-  [/^NCIT:(.+)/i, 'https://ncithesaurus.nci.nih.gov/ncitbrowser/ConceptReport.jsp?dictionary=NCI_Thesaurus&ns=ncit&code='],
+  [
+    /^NCIT:(.+)/i,
+    'https://ncithesaurus.nci.nih.gov/ncitbrowser/ConceptReport.jsp?dictionary=NCI_Thesaurus&ns=ncit&code=',
+  ],
   [/^DRUGBANK:(.+)/i, 'https://go.drugbank.com/drugs/'],
   [/^WIKIDATA:(.+)/i, 'https://www.wikidata.org/wiki/'],
   [/^CHEMIDPLUS:(.+)/i, 'https://pubchem.ncbi.nlm.nih.gov/#query='],
-  [/^IUPHAR.LIGAND:(.+)/i, 'https://www.guidetopharmacology.org/GRAC/LigandDisplayForward?ligandId='],
-  [/^RXCUI:(.+)/i, 'https://mor.nlm.nih.gov/RxNav/search?searchBy=RXCUI&searchTerm='],
+  [
+    /^IUPHAR.LIGAND:(.+)/i,
+    'https://www.guidetopharmacology.org/GRAC/LigandDisplayForward?ligandId=',
+  ],
+  [
+    /^RXCUI:(.+)/i,
+    'https://mor.nlm.nih.gov/RxNav/search?searchBy=RXCUI&searchTerm=',
+  ],
 ];
 
 const genePatterns: [RegExp, string][] = [
   [/^NCBIGENE:(.+)/i, 'https://www.ncbi.nlm.nih.gov/gene/?term='],
-  [/^ENSEMBL:(.+)/i, 'https://ensembl.org/Homo_sapiens/Gene/Summary?db=core;g='],
-  [/^HGNC:(.+)/i, 'https://www.genenames.org/data/gene-symbol-report/#!/hgnc_id/HGNC:'],
+  [
+    /^ENSEMBL:(.+)/i,
+    'https://ensembl.org/Homo_sapiens/Gene/Summary?db=core;g=',
+  ],
+  [
+    /^HGNC:(.+)/i,
+    'https://www.genenames.org/data/gene-symbol-report/#!/hgnc_id/HGNC:',
+  ],
   [/^UCSC:(.+)/, 'https://genome.cse.ucsc.edu/cgi-bin/hgGene?hgg_gene='],
   [
     /^CCDS:(.+)/,
@@ -36,10 +51,17 @@ const genePatterns: [RegExp, string][] = [
   [/^ENA\.EMBL:(.+)/, 'https://www.ebi.ac.uk/ena/browser/view/'],
   [/^CIVIC\.GID:(.+)/, 'https://civicdb.org/genes/'],
   [/^CHEMBL:(.+)/, 'https://www.ebi.ac.uk/chembl/target_report_card/'],
-  [/^IUPHAR\.RECEPTOR:(.+)/, 'https://www.guidetopharmacology.org/GRAC/ObjectDisplayForward?objectId='],
+  [
+    /^IUPHAR\.RECEPTOR:(.+)/,
+    'https://www.guidetopharmacology.org/GRAC/ObjectDisplayForward?objectId=',
+  ],
 ];
 
-export const generateXrefLink = (xref: string, itemType: ResultTypes, linkClassName: string = '') => {
+export const generateXrefLink = (
+  xref: string,
+  itemType: ResultTypes,
+  linkClassName: string = ''
+) => {
   if (!xref.includes(':')) {
     return <>{xref}</>;
   }
