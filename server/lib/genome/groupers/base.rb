9b893fff75a2f84b37549b812aebb042015d7b10
James Stevenson <james.stevenson@nationwidechildrens.org>
2023-06-05 14:04:28 -0400

    fix: use vote-counting to handle ambiguous drug/gene grouping (#391)

--- server/lib/genome/groupers/base.rb
+++ server/lib/genome/groupers/base.rb
@@ -30,23 +30,65 @@ module Genome
         body['source_matches'].reduce({}) { |map, source| map.update(source['source'] => source['source_meta_']) }
       end
 
-      # Normalize claim terms.
+      # Normalize claim terms
+      # If the primary term normalizes successfully, go with that. Otherwise, iterate
+      # through claims and tally all 'votes' for normalized concepts with the highest match
+      # type. For example:
+      # 1) if a gene has three aliases, and two match as `ALIASES` to gene A and the third
+      # matches as an `ALIAS` to gene B, we pick gene A
+      # 2) if a gene has five aliases, and one alias matches as a `LABEL` to gene A, but
+      # the other four aliases match as `ALIASES` to gene B, we pick gene A, since `LABEL`
+      # is the highest match type and there's no tie
+      # 3) if a gene has five aliases, and two match as `LABELS` to different genes, we then
+      # try to break the tie by looking at the votes for the next-best kind of match
+      # 4) if a gene has four aliases, and two match as `LABELS` to different genes, and
+      # the other two match as `ALIASES` to two different genes, we can't break any ties.
+      # So, we just take the first `LABEL` match we saw (arbitrarily).
       def normalize_claim(primary_term, claim_aliases)
         primary_term_upcase = primary_term.upcase
         return @term_to_match_dict[primary_term_upcase] if key_non_nil_match(primary_term_upcase)
 
         response = retrieve_normalizer_response(primary_term)
         if response.nil? || response['match_type'].zero?
-          best_match = nil
-          best_match_type = 0
+          claim_responses = Hash.new
+          match_votes = {
+            100 => Hash.new(0),
+            80 => Hash.new(0),
+            60 => Hash.new(0),
+          }
           claim_aliases.each do |claim_alias|
             response = retrieve_normalizer_response(claim_alias.alias)
-            if !response.nil? && response['match_type'] > best_match_type
-              best_match = response
-              best_match_type = response['match_type']
+            match_type = response['match_type']
+            if !response.nil? && match_type > 0
+              concept_id = response[@descriptor_name][@id_name]
+              if !claim_responses.key?(concept_id)
+                claim_responses[concept_id] = response
+              end
+              match_votes[match_type][concept_id] += 1
             end
           end
-          response = best_match
+
+          # tally votes
+          best_default = nil
+          best_match = nil
+          match_votes.keys.sort.reverse.each do |match_type|
+            next if !best_match.nil?
+            most_votes = match_votes[match_type].values.max
+            next if most_votes.nil?
+            highest_vote_getters = match_votes[match_type].select {|_key, value| value == most_votes}.keys
+            if highest_vote_getters.length == 1
+              best_match = highest_vote_getters[0]
+            elsif highest_vote_getters.length > 1 && best_default.nil?
+              best_default = highest_vote_getters[0]
+            end
+          end
+          if !best_match.nil?
+            response = claim_responses[best_match]
+          elsif !best_default.nil?
+            response = claim_responses[best_default]
+          else
+            response = nil
+          end
         end
         response
       end
