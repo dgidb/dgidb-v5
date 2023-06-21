4272770b932c4837c1d9fece8310a29b72138419
Adam Coffman <acoffman@wustl.edu>
2023-06-21 11:11:54 -0500

    remove debugging prints

--- server/config/deploy/production.rb
+++ server/config/deploy/production.rb
@@ -1,16 +1,14 @@
 server "52.36.252.244", user: 'ubuntu', roles: %w{web db app}
 
-set :branch, 'deployment-automation'
+set :branch, 'main'
 set :rbenv_ruby, '3.1.4'
 
 set :rails_env, 'production'
 
 set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
-puts ENV['DGIDB_STAGING_KEY']
-
 set :ssh_options, {
-    keys: [ENV['DGIDB_PRODUCTION_KEY']],
+    keys: [ENV['DGIDB_PROD_KEY']],
     forward_agent: false,
     auth_methods: %w(publickey)
 }
