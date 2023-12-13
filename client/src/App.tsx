8e21042b5d9bc22dac5409e5ceb508ff98173c44
mcannon068nw <Matthew.Cannon2@nationwidechildrens.org>
2023-12-13 12:57:07 -0500

    logic gate for analytics only in certain environment TODO: set specific variables rather than just REACT URI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,14 @@ import { AppProvider } from './providers/app';
 import { AppRoutes } from './routes';
 import ReactGA from 'react-ga4';
 
+const isProd = process.env.REACT_APP_API_URI === 'http://127.0.0.1:3000/api/graphql'
 const TRACKING_ID = "G-B03N45K7C1"
 
 function App() {
 
-  ReactGA.initialize(TRACKING_ID)
+  if (isProd) {
+    ReactGA.initialize(TRACKING_ID)
+  };
 
   return (
     <AppProvider>
