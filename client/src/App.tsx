d3f560aa2e4380135c802d71653d332a751b07f1
mcannon068nw <Matthew.Cannon2@nationwidechildrens.org>
2023-12-13 12:14:01 -0500

    implemented google analytics (GA4, react-ga4)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 import { AppProvider } from './providers/app';
 import { AppRoutes } from './routes';
+import ReactGA from 'react-ga4';
+
+const TRACKING_ID = "G-B03N45K7C1"
 
 function App() {
+
+  ReactGA.initialize(TRACKING_ID)
+
   return (
     <AppProvider>
       <AppRoutes />
