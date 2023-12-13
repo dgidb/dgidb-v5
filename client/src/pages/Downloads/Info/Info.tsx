876f71230195c48cffabc243077b7c597442e669
Matthew Cannon <87494086+mcannon068nw@users.noreply.github.com>
2023-12-13 10:05:12 -0500

    Dev (#464)
    * feat: update API page to show users endpoint for graphql requests
    
    * feat: update API page to show users endpoint for graphql requests
    
    * feat: update API page to show users endpoint for graphql requests
    
    * feat: update API page to show users endpoint for graphql requests
    
    * feat: add 404 error page to drugs, genes, and interactions that have no data (#456)
    
    * feat: update API page to show users endpoint for graphql requests
    
    * feat: update API page to show users endpoint for graphql requests
    
    * feat: update API page to show users endpoint for graphql requests
    
    * feat: add error message when data not found and fixing footer position
    
    * feat: add error message when data not found
    
    * style: removing console log
    
    * cleaning up code
    
    * refactor: extracting variable for interaction
    
    * feat: add 404 error page to drugs, genes, and interactions that have no data
    
    * add public/data as shared, linked dir
    
    * Update dgidb:load_local method (#463)
    
    * update file download location for dgidb_v5_latest in load_local methods
    
    * update download instructions with rake dgidb:load_local method
    
    * removed psql command in favor of just dgidb:load_local
    
    * fix spacing issue
    
    * prettier
    
    * add December 2023 download links
    
    ---------
    
    Co-authored-by: Katie Stahl <kathryn.stahl@nationwidechildrens.org>
    Co-authored-by: Adam Coffman <acoffman@wustl.edu>

--- client/src/pages/Downloads/Info/Info.tsx
+++ client/src/pages/Downloads/Info/Info.tsx
@@ -11,19 +11,12 @@ export const Info = () => {
         information for each source.
       </p>
       <p>
-        You may load the{' '}
-        <Link
-          href="https://nch-igm-wagner-lab-public.s3.us-east-2.amazonaws.com/dgidb_v5_latest.sql"
-          target="_blank"
-          rel="noopener"
-        >
-          latest data dump
-        </Link>{' '}
-        into your local database instance by running the following command while
-        in your local checkout of the DGIdb repository:
+        You may instantiate your local database instance by running the
+        following command while in the 'server' directory of your local checkout
+        of the DGIdb repository:
       </p>
       <Box sx={{ ml: 2 }} className="code-text-container">
-        <code className="code-text">psql -d dgidb -f dgidb_v5_latest.sql</code>
+        <code className="code-text">rake dgidb:load_local</code>
       </Box>
       <p>
         This will recreate the local database and import the latest data dump
