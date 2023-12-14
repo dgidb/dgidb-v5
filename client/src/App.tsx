be884d7e49df8bc799426b87a8f99424a6684a2f
mcannon068nw <Matthew.Cannon2@nationwidechildrens.org>
2023-12-14 10:44:54 -0500

    implement analytics variable

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,13 @@ import { AppProvider } from './providers/app';
 import { AppRoutes } from './routes';
 import ReactGA from 'react-ga4';
 
-const isProd = process.env.REACT_APP_API_URI === 'http://127.0.0.1:3000/api/graphql'
+const sendAnalytics = process.env.REACT_APP_ANALYTICS === "true"
+
 const TRACKING_ID = "G-B03N45K7C1"
 
 function App() {
 
-  if (isProd) {
+  if (sendAnalytics) {
     ReactGA.initialize(TRACKING_ID)
   };
 
