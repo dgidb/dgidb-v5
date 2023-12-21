215559422bbacfbee70a36fe71c733ecb40aa478
mcannon068nw <Matthew.Cannon2@nationwidechildrens.org>
2023-12-21 10:35:01 -0500

    wip figuring out graphql context

--- server/app/controllers/graphql_controller.rb
+++ server/app/controllers/graphql_controller.rb
@@ -10,9 +10,13 @@ class GraphqlController < ApplicationController
     operation_name = params[:operationName]
     context = {
       # Query context goes here, for example:
-      # current_user: current_user,
+      test_query: "this is only a test",
+      query_structure: query,
+      query_variables: variables
     }
     result = DgidbSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    Rails.logger.debug context
+
     render json: result
   rescue StandardError => e
     raise e unless Rails.env.development?
