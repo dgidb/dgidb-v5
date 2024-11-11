6f997138142a1aa695a495d0f379445237c62aea
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-11-11 12:50:35 -0500

    ope forgot to check request

--- server/app/lib/api_analytics_tracer.rb
+++ server/app/lib/api_analytics_tracer.rb
@@ -9,7 +9,7 @@ module ApiAnalyticsTracer
       user_ip: query.context[:request_ip],
       query: query.query_string.squish,
       query_variables: query.provided_variables,
-      client_type: request.headers['dgidb-client-name']
+      client_type: query.context[:client_name]
     }
 
     API_LOGGER.info(params)
