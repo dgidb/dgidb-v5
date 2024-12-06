d336ae0b5f1a7f8deeea15de48b1abb4be5b2302
Cannon <mcannon068nw@gmail.com>
2024-12-06 10:23:45 -0500

    new row for old Downloads file

--- client/src/pages/Downloads/Files/Files.tsx
+++ client/src/pages/Downloads/Files/Files.tsx
@@ -25,6 +25,7 @@ function getDataObj(date: string) {
 
 const rows = [
   getDataObj('latest'),
+  getDataObj('2024-Jun'),
   getDataObj('2023-Dec'),
   getDataObj('2022-Feb'),
   getDataObj('2021-May'),
