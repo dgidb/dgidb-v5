fbc359008a51b60962fec69211c0d1b60d029193
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-04-18 09:19:26 -0400

    more refactoring

---
+++ client/src/components/About/NoteBox/NoteBox.tsx
@@ -0,0 +1,13 @@
+import { Paper } from "@mui/material";
+import React, { ReactNode } from "react";
+import './NoteBox.scss';
+
+interface NoteBoxProps {
+    children: ReactNode;
+}
+
+export const NoteBox: React.FC<NoteBoxProps> = ({ children }) => (
+    <Paper className="about-note-box">
+        {children}
+    </Paper>
+)
