af8e1985cbd44269c11b98f8f7704446d1fbdfc9
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-04-23 12:28:37 -0400

    feat: add stats api endpoint

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  post "/api/graphql", to: "graphql#execute"
-  post "/api/graphql/download/:table_name", to: "table_download#download_table"
+  post '/api/graphql', to: 'graphql#execute'
+  post '/api/graphql/download/:table_name', to: 'table_download#download_table'
 
-  mount GraphiQL::Rails::Engine, at: "/api/graphiql", graphql_path: "/api/graphql"
+  get '/api/counts', to: 'counts#index'
+
+  mount GraphiQL::Rails::Engine, at: '/api/graphiql', graphql_path: '/api/graphql'
 end
