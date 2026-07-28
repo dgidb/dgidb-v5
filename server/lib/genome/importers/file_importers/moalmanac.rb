02ffc29945d95c9117d63db3731b065ffdb7321c
James Stevenson <james.stevenson@nationwidechildrens.org>
2026-07-28 12:49:53 -0400

    add stuff

--- server/lib/genome/importers/file_importers/moalmanac.rb
+++ server/lib/genome/importers/file_importers/moalmanac.rb
@@ -1,9 +1,12 @@
-require 'JSON'
+# frozen_string_literal: true
+
+require 'json'
 
 module Genome
   module Importers
     module FileImporters
       module Moalmanac
+        # Imports interaction claims from MOAlmanac JSON exports.
         class Importer < Genome::Importers::Base
           attr_reader :file_path
 
@@ -33,14 +36,14 @@ module Genome
           def create_new_source
             @source ||= Source.create(
               {
-                base_url: 'TODO',
+                base_url: 'https://dev.moalmanac.org/statements/',
                 site_url: 'https://moalmanac.org/',
                 citation: 'Reardon B, Moore ND, Moore NS, Kofman E, AlDubayan SH, Cheung ATM, Conway J, Elmarakeby H, Imamovic A, Kamran SC, Keenan T, Keliher D, Konieczkowski DJ, Liu D, Mouw KW, Park J, Vokes NI, Dietlein F, Van Allen EM. Integrating molecular profiles into clinical frameworks through the Molecular Oncology Almanac to prospectively guide precision oncology. Nat Cancer. 2021 Oct;2(10):1102-1112. doi: 10.1038/s43018-021-00243-3. Epub 2021 Sep 30. PMID: 35121878; PMCID: PMC9082009.',
                 citation_short: 'Reardon B, et al. Integrating molecular profiles into clinical frameworks through the Molecular Oncology Almanac to prospectively guide precision oncology. Nat Cancer. 2021 Oct;2(10):1102-1112.',
                 pmid: '35121878',
                 pmcid: 'PMC9082009',
                 doi: '10.1038/s43018-021-00243-3',
-                source_db_version: @data_version, # TODO
+                source_db_version: @data_version,
                 source_db_name: source_db_name,
                 full_name: 'Molecular Oncology Almanac',
                 license: 'ODbL',
@@ -55,9 +58,16 @@ module Genome
             drug_claim = create_drug_claim(drug_record['name'], DrugNomenclature::TRADE_NAME)
             create_drug_claim_alias(drug_claim, "moa.tid:#{drug_record['id']}", DrugNomenclature::MOA_TID)
             create_drug_claim_alias(drug_claim, drug_record['primaryCoding']['id'], DrugNomenclature::NCIT_ID)
-            # drug_record['extensions'].each do |extension|
-            #   create_drug_claim_attribute(drug_claim, extension['name'], extension['value'])
-            # end
+            drug_record['extensions'].each do |extension|
+              case extension['value']
+              when String
+                create_drug_claim_attribute(drug_claim, extension['name'], extension['value'])
+              when Array
+                extension['value'].each do |value|
+                  create_drug_claim_attribute(drug_claim, extension['name'], value) if value.is_a?(String)
+                end
+              end
+            end
             drug_claim
           end
 
@@ -72,6 +82,17 @@ module Genome
             drug_claim = ingest_drug(drug_record)
             interaction_claim = create_interaction_claim(gene_claim, drug_claim)
             create_interaction_claim_link(interaction_claim, 'MOAlmanac Statement', "https://dev.moalmanac.org/statements/#{statement['id']}")
+            if statement['proposition']['objectTherapeutic'].key? 'membershipOperator'
+              combo_therapy_value = statement['proposition']['objectTherapeutic']['therapies'].filter_map do |obj|
+                obj['name']
+              end.join(', ')
+              create_interaction_claim_attribute(interaction_claim, InteractionAttributeName::COMBINATION,
+                                                 combo_therapy_value)
+            end
+            return unless statement.key? 'indication'
+
+            create_interaction_claim_attribute(interaction_claim, InteractionAttributeName::INDICATION,
+                                               statement['indication']['description'])
           end
 
           def create_interaction_claims
@@ -83,17 +104,50 @@ module Genome
                                therapeutic['therapies']
                              else
                                [therapeutic]
-
                              end
 
-              # TODO: figure out complex catvars/fusions/etc
               biomarkers = statement['proposition']['biomarkers']
-              next unless biomarkers.one?
+              unless biomarkers.one?
+                # these might not be meaningful for our purposes, so we want to ignore them for now
+                ignored_biomarkers = [
+                  'HER2-negative',
+                  'ER positive',
+                  'ER negative',
+                  'PR positive',
+                  'PR negative',
+                  'CD19 +',
+                  'CD22 +',
+                  'dMMR'
+                ]
+                ignored_patterns = [
+                  /\APD-L1.*>=/
+                ]
+                biomarkers = biomarkers.reject do |biomarker|
+                  name = biomarker['name']
+
+                  name &&
+                    (
+                      ignored_biomarkers.include?(name) ||
+                      ignored_patterns.any? { |pattern| name.match?(pattern) }
+                    )
+                end
+                next unless biomarkers.one?
+              end
 
               genes = biomarkers.first['genes']
-              next unless genes&.one?
 
-              gene_records = [genes.first]
+              if genes&.length != 1
+                next if genes.nil?
+
+                # special case -- pull out ABL1 interaction for BCR-ABL1 fusions
+                next unless genes.map { |g| g['name'] }.sort == %w[ABL1 BCR]
+
+                gene_records = [genes.find { |g| g['name'] == 'ABL1' }]
+              elsif genes&.length == 1
+                gene_records = genes
+              else
+                next
+              end
 
               drug_records.product(gene_records).each do |drug_record, gene_record|
                 ingest_interaction(gene_record, drug_record, statement)
